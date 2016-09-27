d50d59c9b6c9b47fb3881d5febe3997e4606e8b0
BoykoAlex <aboyko@pivotal.io>
2016-09-27 13:09:51 -0400

    Missing refactoring change

--- vscode-extensions/commons/java-properties/src/test/java/org/springframework/ide/vscode/properties/parser/test/PropertiesAntlrParserTest.java
+++ vscode-extensions/commons/java-properties/src/test/java/org/springframework/ide/vscode/properties/parser/test/PropertiesAntlrParserTest.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Pivotal, Inc. - initial API and implementation
  *******************************************************************************/
-package org.springframework.ide.vscode.parser.test;
+package org.springframework.ide.vscode.properties.parser.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
