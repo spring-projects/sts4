31510f3fc2441ad3afdde4ea2e947834f9abdf10
Martin Lippert <mlippert@pivotal.io>
2016-11-30 12:45:53 +0100

    first steps towards installable eclipse features for language servers

---
+++ eclipse-language-servers/org.springframework.boot.ide.properties.servers/src/org/springframework/boot/ide/properties/servers/Constants.java
@@ -0,0 +1,7 @@
+package org.springframework.boot.ide.properties.servers;
+
+public class Constants {
+	
+	public static final String PLUGIN_ID = "org.springframework.boot.ide.properties.servers";
+
+}
--- vscode-extensions/vscode-application-properties/scripts/preinstall.sh
+++ vscode-extensions/vscode-application-properties/scripts/preinstall.sh
@@ -2,4 +2,4 @@
 set -e
 (cd ../commons-vscode ; npm install)
 npm install ../commons-vscode
-../mvnw -f ../pom.xml -pl vscode-application-properties -am clean package
+../mvnw -f ../pom.xml -pl vscode-application-properties -am clean install
