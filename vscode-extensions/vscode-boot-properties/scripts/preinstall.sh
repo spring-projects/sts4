262b26e78cbb759e0d25034d4444ad90addb2fc9
Kris De Volder <kdevolder@pivotal.io>
2016-11-30 16:47:51 -0800

    One more try to fix tycho build

--- vscode-extensions/vscode-boot-properties/scripts/preinstall.sh
+++ vscode-extensions/vscode-boot-properties/scripts/preinstall.sh
@@ -2,4 +2,4 @@
 set -e
 (cd ../commons-vscode ; npm install)
 npm install ../commons-vscode
-../mvnw -U -f ../pom.xml -pl vscode-boot-properties -am clean package
+../mvnw -U -f ../pom.xml -pl vscode-boot-properties -am clean install
