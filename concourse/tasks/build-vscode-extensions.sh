16218fcc660a0b1dc2e848feebdeb39b8d036b4d
Kris De Volder <kdevolder@pivotal.io>
2016-12-08 15:33:41 -0800

    A few little fixes

--- concourse/tasks/build-vscode-extensions.sh
+++ concourse/tasks/build-vscode-extensions.sh
@@ -6,4 +6,5 @@ cd sts4/vscode-extensions
 ./build-all.sh
 
 cd $workdir
-mv sts4/vscode-extensions/vscode-*/target/*.vsix vsix-files/
+mv sts4/vscode-extensions/vscode-manifest-yaml/target/*.vsix vsix-files/
+mv sts4/vscode-extensions/vscode-boot-properties/target/*.vsix vsix-files/
