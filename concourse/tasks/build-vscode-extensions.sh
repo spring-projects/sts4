77884a00ba3df26749ada7cffbb87ce302b9162a
Kris De Volder <kdevolder@pivotal.io>
2016-12-08 16:00:29 -0800

    Try to fix problem copying build .vsix files

--- concourse/tasks/build-vscode-extensions.sh
+++ concourse/tasks/build-vscode-extensions.sh
@@ -6,5 +6,5 @@ cd sts4/vscode-extensions
 ./build-all.sh
 
 cd $workdir
-mv sts4/vscode-extensions/vscode-manifest-yaml/target/*.vsix vsix-files/
-mv sts4/vscode-extensions/vscode-boot-properties/target/*.vsix vsix-files/
+cp sts4/vscode-extensions/vscode-manifest-yaml/*.vsix vsix-files/
+cp sts4/vscode-extensions/vscode-boot-properties/*.vsix vsix-files/
