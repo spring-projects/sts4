27c7e9fefda28713aa5fe5a6a55721013c19cda8
Kris De Volder <kdevolder@pivotal.io>
2017-01-12 13:39:11 -0800

    Run script to fix remaining copyrights

--- fix-copyright.sh
+++ fix-copyright.sh
@@ -3,5 +3,6 @@ grep -r --exclude-dir=resources --exclude-dir=target -L --include="*.java" "\* C
 
 for i in `cat missing-copyright.txt` ; do
    echo Fixing $i
-   cat java-copyright-header.txt "$i" > $i
+   cat java-copyright-header.txt "$i" > $i.tmp
+   mv $i.tmp $i
 done
--- vscode-extensions/commons/commons-util/src/main/java/org/springframework/ide/vscode/commons/util/BadLocationException.java
+++ vscode-extensions/commons/commons-util/src/main/java/org/springframework/ide/vscode/commons/util/BadLocationException.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2016-2017 Pivotal, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Pivotal, Inc. - initial API and implementation
+ *******************************************************************************/
+
 package org.springframework.ide.vscode.commons.util;
 
 /**
