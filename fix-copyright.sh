36571a0a3f754e93576baca4d06b44806c2495a8
Kris De Volder <kdevolder@pivotal.io>
2017-01-12 13:32:18 -0800

    Add script for adding missing copyrights in bulk

---
+++ fix-copyright.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+grep -r --exclude-dir=resources --exclude-dir=target -L --include="*.java" "\* Copyright" > missing-copyright.txt
+
+for i in `cat missing-copyright.txt` ; do
+   echo Fixing $i
+   cat java-copyright-header.txt "$i" > $i
+done
