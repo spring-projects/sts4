258ac089b7bbf59bd368a2beed4b54a38da8d449
Kris De Volder <kdevolder@pivotal.io>
2017-01-19 14:29:31 -0800

    Change language-id for manifest.yml files

--- vscode-extensions/vscode-manifest-yaml/scripts/preinstall.sh
+++ vscode-extensions/vscode-manifest-yaml/scripts/preinstall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
+# Download yaml TextMate grammar
+curl https://raw.githubusercontent.com/textmate/yaml.tmbundle/master/Syntaxes/YAML.tmLanguage > yaml-support/yaml.tmLanguage
+
+# Preinstall commons-vscode package
 (cd ../commons-vscode ; npm install)
 npm install ../commons-vscode
+
+# Use maven to build fat jar of the language server
 ../mvnw -U -f ../pom.xml -pl vscode-manifest-yaml -am clean install
+
