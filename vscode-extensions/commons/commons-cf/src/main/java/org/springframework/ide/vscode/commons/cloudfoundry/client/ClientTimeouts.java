80716fe8b883170811638afd5613897c4b4a2f40
nsingh <nsingh@pivotal.io>
2017-01-24 10:32:03 -0800

    Removed unused timeouts

--- vscode-extensions/commons/commons-cf/src/main/java/org/springframework/ide/vscode/commons/cloudfoundry/client/ClientTimeouts.java
+++ vscode-extensions/commons/commons-cf/src/main/java/org/springframework/ide/vscode/commons/cloudfoundry/client/ClientTimeouts.java
@@ -13,31 +13,14 @@ package org.springframework.ide.vscode.commons.cloudfoundry.client;
 import java.time.Duration;
 
 public class ClientTimeouts {
-	
+
 	public static final ClientTimeouts DEFAULT_TIMEOUTS = new ClientTimeouts();
-	
-	private static final Duration APP_START_TIMEOUT = Duration.ofMillis(60*10);
 	private static final Duration GET_SERVICES_TIMEOUT = Duration.ofSeconds(60);
-	private static final Duration GET_SPACES_TIMEOUT = Duration.ofSeconds(20);
-	private static final Duration GET_USERNAME_TIMEOUT = Duration.ofSeconds(5);
-  
-	
+
 	public Duration getServicesTimeout() {
 		return GET_SERVICES_TIMEOUT;
 	}
-	
-	public Duration getSpacesTimeout() {
-		return GET_SPACES_TIMEOUT;
-	}
-	
-	public Duration getUsernameTimeout() {
-		return GET_USERNAME_TIMEOUT;
-	}
-	
-	public Duration getAppStartTimeout() {
-		return APP_START_TIMEOUT;
-	}
-	
+
 	public Duration getBuildpacksTimeout() {
 		return Duration.ofSeconds(10);
 	}
