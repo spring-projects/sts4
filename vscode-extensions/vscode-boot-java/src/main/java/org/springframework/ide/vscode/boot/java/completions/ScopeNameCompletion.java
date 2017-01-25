5b010422066786dc1fc3e944d0b580261e93b56b
Martin Lippert <mlippert@pivotal.io>
2017-01-25 20:57:34 +0100

    first experiments towards spring-specific content-assist for spring annotations, starting with scope

---
+++ vscode-extensions/vscode-boot-java/src/main/java/org/springframework/ide/vscode/boot/java/completions/ScopeNameCompletion.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Pivotal, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Pivotal, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.vscode.boot.java.completions;
+
+import org.eclipse.lsp4j.CompletionItemKind;
+import org.springframework.ide.vscode.commons.util.Renderable;
+
+/**
+ * @author Martin Lippert
+ */
+public class ScopeNameCompletion {
+	
+	private final String label;
+	private final String detail;
+	private final Renderable documentation;
+	private final CompletionItemKind kind;
+	private final String value;
+	
+	public ScopeNameCompletion(String value, String label, String detail, Renderable documentation, CompletionItemKind kind) {
+		super();
+		this.value = value;
+		this.label = label;
+		this.detail = detail;
+		this.documentation = documentation;
+		this.kind = kind;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public String getDetail() {
+		return detail;
+	}
+	
+	public CompletionItemKind getKind() {
+		return kind;
+	}
+	
+	public Renderable getDocumentation() {
+		return documentation;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+
+}
---
+++ vscode-extensions/vscode-boot-java/src/main/java/org/springframework/ide/vscode/boot/java/completions/ScopeNameCompletionProposal.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Pivotal, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Pivotal, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.vscode.boot.java.completions;
+
+import org.eclipse.lsp4j.CompletionItemKind;
+import org.springframework.ide.vscode.commons.languageserver.completion.DocumentEdits;
+import org.springframework.ide.vscode.commons.languageserver.completion.ICompletionProposal;
+import org.springframework.ide.vscode.commons.util.Renderable;
+import org.springframework.ide.vscode.commons.util.text.IDocument;
+
+/**
+ * @author Martin Lippert
+ */
+public class ScopeNameCompletionProposal implements ICompletionProposal {
+	
+	public static final ScopeNameCompletion[] COMPLETIONS = new ScopeNameCompletion[] {
+			new ScopeNameCompletion("\"prototype\"", "prototype", "prototype scope", null, CompletionItemKind.Value),
+			new ScopeNameCompletion("\"singleton\"", "singleton", "singleton scope (default)", null, CompletionItemKind.Value),
+			new ScopeNameCompletion("\"request\"", "request", "request scope", null, CompletionItemKind.Value),
+			new ScopeNameCompletion("\"session\"", "session", "session scope", null, CompletionItemKind.Value),
+			new ScopeNameCompletion("\"globalSession\"", "globalSession", "globalSession scope", null, CompletionItemKind.Value),
+			new ScopeNameCompletion("\"application\"", "application", "application scope", null, CompletionItemKind.Value),
+			new ScopeNameCompletion("\"websocket\"", "websocket", "websocket scope", null, CompletionItemKind.Value)
+	}; 
+	
+	private final IDocument doc;
+	private final int startOffset;
+	private final int endOffset;
+	private final ScopeNameCompletion completion;
+	private final String prefix;
+
+	public ScopeNameCompletionProposal(ScopeNameCompletion completion, IDocument doc, int startOffset, int endOffset, String prefix) {
+		this.completion = completion;
+		this.doc = doc;
+		this.startOffset = startOffset;
+		this.endOffset = endOffset;
+		this.prefix = prefix;
+	}
+
+	@Override
+	public ICompletionProposal deemphasize() {
+		return null;
+	}
+
+	@Override
+	public String getLabel() {
+		return completion.getLabel();
+	}
+
+	@Override
+	public CompletionItemKind getKind() {
+		return completion.getKind();
+	}
+
+	@Override
+	public DocumentEdits getTextEdit() {
+		DocumentEdits edits = new DocumentEdits(doc);
+		edits.replace(startOffset + prefix.length(), endOffset, completion.getValue().substring(prefix.length()));
+		return edits;
+	}
+
+	@Override
+	public String getDetail() {
+		return completion.getDetail();
+	}
+
+	@Override
+	public Renderable getDocumentation() {
+		return completion.getDocumentation();
+	}
+
+}
