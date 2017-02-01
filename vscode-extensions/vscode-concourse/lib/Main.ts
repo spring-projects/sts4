bfbc8e5083988bb7f0fac2f84497610d545cd35b
Kris De Volder <kdevolder@pivotal.io>
2017-02-01 10:06:32 -0800

    Adjust jvm heap sizes for vscode extensions about to be released.

--- vscode-extensions/vscode-concourse/lib/Main.ts
+++ vscode-extensions/vscode-concourse/lib/Main.ts
@@ -35,6 +35,7 @@ export function activate(context: VSCode.ExtensionContext) {
         CONNECT_TO_LS: false,
         extensionId: 'vscode-concourse',
         fatJarFile: 'target/vscode-concourse-0.0.1-SNAPSHOT.jar',
+        jvmHeap: "48m",
         clientOptions: {
             documentSelector: [ PIPELINE_LANGUAGE_ID, TASK_LANGUAGE_ID ],
             synchronize: {
