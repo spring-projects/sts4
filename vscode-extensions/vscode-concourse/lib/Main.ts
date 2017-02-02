914f074ae29bb369bf16809cbed44eb14acd8c3e
Kris De Volder <kdevolder@pivotal.io>
2017-02-02 10:05:47 -0800

    Bump version number in concourse language server pom

--- vscode-extensions/vscode-concourse/lib/Main.ts
+++ vscode-extensions/vscode-concourse/lib/Main.ts
@@ -34,7 +34,7 @@ export function activate(context: VSCode.ExtensionContext) {
         DEBUG : false,
         CONNECT_TO_LS: false,
         extensionId: 'vscode-concourse',
-        fatJarFile: 'target/vscode-concourse-0.0.1-SNAPSHOT.jar',
+        fatJarFile: 'target/vscode-concourse-0.0.2-SNAPSHOT.jar',
         jvmHeap: "48m",
         clientOptions: {
             documentSelector: [ PIPELINE_LANGUAGE_ID, TASK_LANGUAGE_ID ],
