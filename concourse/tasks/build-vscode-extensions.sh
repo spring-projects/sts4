2b671d1a1e6576fd894f407b439c3473a596c5bc
Kris De Volder <kdevolder@pivotal.io>
2017-04-04 08:07:03 -0700

    Attempt to speedup build by prepopulating maven cache

--- concourse/tasks/build-vscode-extensions.sh
+++ concourse/tasks/build-vscode-extensions.sh
@@ -2,6 +2,14 @@
 set -e
 workdir=`pwd`
 
+if [ -d "maven-cache" ]; then
+    echo "Prepopulating maven cache"
+    tar xzvf maven-cache/*.tar.gz -C ${HOME}
+else
+    echo "!!!No stored maven cache found!!! "
+    echo "!!!This may slow down the build!!!"
+fi
+
 cd sts4/vscode-extensions
 ./build-all.sh
 
