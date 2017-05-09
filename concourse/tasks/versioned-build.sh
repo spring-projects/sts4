09ee81301d72531fdb9590c938ebb55ebf3b405c
Kris De Volder <kdevolder@pivotal.io>
2017-05-09 16:08:13 -0700

    Fix more problems in RC build

--- concourse/tasks/versioned-build.sh
+++ concourse/tasks/versioned-build.sh
@@ -8,11 +8,6 @@ version=`cat version/version`
 echo "extension_id=${extension_id}"
 echo "version=${version}"
 
-cd "sts4/vscode-extensions/${extension_id}"
-npm version "${version}"
-echo "Version set to " `npm version`
-echo -e "\n\n*Version: ${version}*" >> README.md
-
 if [ -d "maven-cache" ]; then
     echo "Prepopulating maven cache"
     tar xzf maven-cache/*.tar.gz -C ${HOME}
@@ -20,6 +15,11 @@ else
    echo "!!!No stored maven cache found!!! "
    exit -1
 fi
+
+cd "sts4/vscode-extensions/${extension_id}"
+npm version "${version}"
+echo "Version set to " `npm version`
+echo -e "\n\n*Version: ${version}*" >> README.md
 
 # Build commons vscode
 cd ${sources}/../commons-vscode
