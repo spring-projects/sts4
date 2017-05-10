6bfb07eb8cf16b228f466ab815affe3507df104f
Kris De Volder <kdevolder@pivotal.io>
2017-05-10 14:20:40 -0700

    Remove obsolete build scripts and tasks

--- concourse/tasks/create-rc-build-tag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-timestamp=`date -u +%Y%m%d%H%M`
-
-base_dir=`pwd`
-
-cd sts4/vscode-extensions/${extension_id}
-base_version=`jq -r .version package.json`
-
-echo ${extension_id}-${base_version}-RC${timestamp} > ${base_dir}/out/tag
