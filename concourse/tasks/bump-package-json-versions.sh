c2da9f4df85f5154fb6748915db85f4ee46fa481
Kris De Volder <kdevolder@pivotal.io>
2017-05-10 19:07:12 -0700

    Continue fleshing out post-release job
    Try updating package.jsons and commiting the
    change to git.
    
    Use TEST branch for now.

--- concourse/tasks/bump-package-json-versions.sh
+++ concourse/tasks/bump-package-json-versions.sh
@@ -9,31 +9,16 @@ echo "version=$version"
 
 for extension_id in vscode-manifest-yaml vscode-concourse commons-vscode
 do
+    cd $sources/$extension_id
     echo "Should update version of $extension_id to $version"
-    echo "But this isn't implemented yet!"
+    npm version $version
     echo ""
 done
-
-exit 99
-
-release_version=`jq -r .version ${sources}/package.json`
-
-echo "release_version=$release_version"
-
-cd $sources
-
-echo "Bumping version of ${extension_id}"
-echo "release_version=${release_version}"
-npm version patch
-git add .
-
-new_version=`jq -r .version ${sources}/package.json`
-echo "new_version=${new_version}"
 
 git config user.email "kdevolder@pivotal.io"
 git config user.name "Kris De Volder"
 
 git commit \
-    -m "Bump version of ${extension_id} to ${new_version}"
+    -m "Bump version of vscode extensions to ${version}"
 
-git clone $workdir/sts4 $workdir/sts4-out
+git clone $workdir/sts4 $workdir/out
