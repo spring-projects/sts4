e5e70c35c7159d85de57f409eacf937e391736ca
Kris De Volder <kdevolder@pivotal.io>
2017-05-12 14:53:37 -0700

    Release script now 'armed'.

--- concourse/tasks/publish-vsix-releases.sh
+++ concourse/tasks/publish-vsix-releases.sh
@@ -9,12 +9,9 @@ do
     echo "*** Publishing : ${vsix_file}"
     echo "****************************************************************"
     echo ""
-    echo "We should be runing the following command:"
+    echo "We are runing the following command:"
     echo ""
     echo "     vsce publish -p $vsce_token --packagePath $vsix_file"
     echo ""
-    echo "But this is just a practice run..."
-    echo ""
+    vsce publish -p $vsce_token --packagePath $vsix_file
 done
-
-exit 99 ## take this line out once the publishing code is functional and does something real.
