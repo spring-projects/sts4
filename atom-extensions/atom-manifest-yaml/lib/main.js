6e8f7cb264185e99e36282147bba7a4d5ff3a3d6
BoykoAlex <aboyko@pivotal.io>
2017-07-20 16:04:18 -0400

    System property not to remove indentation from completions edits

--- atom-extensions/atom-manifest-yaml/lib/main.js
+++ atom-extensions/atom-manifest-yaml/lib/main.js
@@ -218,7 +218,7 @@ class ManifestYamlLanguageClient extends JarLanguageClient {
 
     constructor() {
         super(
-            'https://s3-us-west-1.amazonaws.com/s3-test.spring.io/sts4/fatjars/snapshots/manifest-yaml-language-server-0.0.9-201707192158.jar',
+            'https://s3-us-west-1.amazonaws.com/s3-test.spring.io/sts4/fatjars/snapshots/manifest-yaml-language-server-0.0.9-201707201812.jar',
             path.join(__dirname, '..', 'server')
         );
 
