f87293303e62b4c6de7d1ea7eea3a4f6d313a547
Kris De Volder <kdevolder@pivotal.io>
2017-08-03 11:26:30 -0700

    Copy fatjars to output folder after atom test build

--- concourse/tasks/atom-language-servers-test.sh
+++ concourse/tasks/atom-language-servers-test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
+set -v
 workdir=`pwd`
 sources=$workdir/sts4/headless-services
+output=$workdir/out
 
 if [ -d "maven-cache" ]; then
     echo "Prepopulating maven cache"
@@ -13,3 +15,11 @@ fi
 
 cd ${sources}
 ./mvnw package -DargLine="-Dlsp.lazy.completions.disable=true -Dlsp.completions.indentation.enable=true -Dlsp.yaml.completions.errors.disable=true"
+
+timestamp=`date -u +%Y%m%d%H%M`
+for i in `ls *-language-server/target/*.jar`; do
+    basename=$basename($i)
+    cp $i $output/${basename/SNAPSHOT/$timestamp}
+done
+
+find . -name "*-language-server"
