3ed4f04f2d2d1c316c715645a1a391d530fbfdac
Kris De Volder <kdevolder@pivotal.io>
2017-08-03 10:28:45 -0700

    Add test run for atom language-server

---
+++ concourse/tasks/atom-language-servers-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+workdir=`pwd`
+sources=$workdir/sts4/headless-services
+
+if [ -d "maven-cache" ]; then
+    echo "Prepopulating maven cache"
+    tar xzf maven-cache/*.tar.gz -C ${HOME}
+else
+   echo "!!!No stored maven cache found!!! "
+   echo "!!!This may slow down the build!!!"
+fi
+
+./mvnw test -DargLine="-Dlsp.lazy.completions.disable=true -Dlsp.completions.indentation.enable=true -Dlsp.yaml.completions.errors.disable=true"
