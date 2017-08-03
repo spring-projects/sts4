cae5e83039489b3dfa9e46491c793aeb97a3ff00
Kris De Volder <kdevolder@pivotal.io>
2017-08-03 11:01:13 -0700

    Run package instead of test for atom-tests build

--- concourse/tasks/atom-language-servers-test.sh
+++ concourse/tasks/atom-language-servers-test.sh
@@ -12,4 +12,4 @@ else
 fi
 
 cd ${sources}
-./mvnw test -DargLine="-Dlsp.lazy.completions.disable=true -Dlsp.completions.indentation.enable=true -Dlsp.yaml.completions.errors.disable=true"
+./mvnw package -DargLine="-Dlsp.lazy.completions.disable=true -Dlsp.completions.indentation.enable=true -Dlsp.yaml.completions.errors.disable=true"
