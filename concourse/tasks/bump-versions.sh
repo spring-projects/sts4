7d2f0d70371b0800465b33d329878a0d7499ec5f
Kris De Volder <kdevolder@pivotal.io>
2017-08-14 14:02:31 -0700

    Tweaks to version bumping scripts

--- concourse/tasks/bump-versions.sh
+++ concourse/tasks/bump-versions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -v
 workdir=`pwd`
 
 sources=$workdir/sts4/vscode-extensions
@@ -7,7 +8,7 @@ sources=$workdir/sts4/vscode-extensions
 version=`cat version/version`
 echo "version=$version"
 
-for extension_id in vscode-manifest-yaml vscode-concourse
+for extension_id in $(ls -d vscode-*)
 do
     cd $sources/$extension_id
     echo "Should update version of $extension_id to $version"
@@ -16,11 +17,13 @@ do
     echo ""
 done
 
+cd $workdir/sts4/headless-services
+$workdir/sts4/concourse/tasks/update-pom-versions.sh $version
+
 git config user.email "kdevolder@pivotal.io"
 git config user.name "Kris De Volder"
 
-cd $workdir/sts4/headless-services
-$workdir/sts4/concourse/tasks/update-pom-versions.sh $version
+git add .
 
 git commit \
     -m "Bump version to ${version}"
