155598bd771bf58919e7c3c02eea133a2ccfb50c
Kris De Volder <kdevolder@pivotal.io>
2017-09-22 11:01:07 -0700

    Add fake reconciler that show some info and warning messages

--- headless-services/boot-java-language-server/src/main/java/org/springframework/ide/vscode/boot/java/handlers/BootJavaReconcileEngine.java
+++ headless-services/boot-java-language-server/src/main/java/org/springframework/ide/vscode/boot/java/handlers/BootJavaReconcileEngine.java
@@ -10,17 +10,48 @@
  *******************************************************************************/
 package org.springframework.ide.vscode.boot.java.handlers;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.ide.vscode.commons.languageserver.reconcile.IProblemCollector;
 import org.springframework.ide.vscode.commons.languageserver.reconcile.IReconcileEngine;
+import org.springframework.ide.vscode.commons.languageserver.reconcile.ProblemSeverity;
+import org.springframework.ide.vscode.commons.languageserver.reconcile.ProblemType;
+import org.springframework.ide.vscode.commons.languageserver.reconcile.ProblemTypes;
+import org.springframework.ide.vscode.commons.languageserver.reconcile.ReconcileProblem;
+import org.springframework.ide.vscode.commons.languageserver.reconcile.ReconcileProblemImpl;
+import org.springframework.ide.vscode.commons.languageserver.util.DocumentRegion;
 import org.springframework.ide.vscode.commons.util.text.IDocument;
+
 
 /**
  * @author Martin Lippert
  */
 public class BootJavaReconcileEngine implements IReconcileEngine {
 
+	private static final Pattern infoWord = Pattern.compile("information|warning");
+
+	private static final ProblemType INFO_AVAILABLE = ProblemTypes.create("BootInfoAvailable", ProblemSeverity.INFO);
+	private static final ProblemType WARNING = ProblemTypes.create("BootInfoAvailable", ProblemSeverity.WARNING);
+
 	@Override
-	public void reconcile(IDocument doc, IProblemCollector problemCollector) {
+	public void reconcile(IDocument _doc, IProblemCollector problemCollector) {
+		problemCollector.beginCollecting();
+		DocumentRegion doc = new DocumentRegion(_doc);
+		try {
+			Matcher matcher = infoWord.matcher(doc);
+			while (matcher.find()) {
+				problemCollector.accept(info(doc.subSequence(matcher.start(), matcher.end())));
+			}
+		} finally {
+			problemCollector.endCollecting();
+		}
 	}
 
+	private ReconcileProblem info(DocumentRegion region) {
+		if (region.toString().contains("info")) {
+			return new ReconcileProblemImpl(INFO_AVAILABLE, "There's dynamic boot info available", region.getStart(), region.getLength());
+		}
+		return new ReconcileProblemImpl(WARNING, "Be warned!", region.getStart(), region.getLength());
+	}
 }
