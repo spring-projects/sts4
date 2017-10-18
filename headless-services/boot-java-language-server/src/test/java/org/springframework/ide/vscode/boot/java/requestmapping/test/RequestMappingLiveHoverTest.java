bd509ed14dd6f0a870594c6ebfb6077a4054f9e3
nsingh <nsingh@pivotal.io>
2017-10-18 13:34:52 -0700

    Added test case to test live hovers over methods

--- headless-services/boot-java-language-server/src/test/java/org/springframework/ide/vscode/boot/java/requestmapping/test/RequestMappingLiveHoverTest.java
+++ headless-services/boot-java-language-server/src/test/java/org/springframework/ide/vscode/boot/java/requestmapping/test/RequestMappingLiveHoverTest.java
@@ -74,12 +74,52 @@ public class RequestMappingLiveHoverTest {
 		harness.intialize(directory);
 
 		Editor editor = harness.newEditorFromFileUri(docUri, LanguageId.JAVA);
-		editor.assertHoverContains("@RequestMapping", "Path: [/hello-world](http://cfapps.io:1111/hello-world)\n" +
+		editor.assertHoverContains("@RequestMapping(\"/hello-world\")", "Path: [/hello-world](http://cfapps.io:1111/hello-world)\n" +
 				"\n" +
 				"Process ID: 22022\n" +
 				"\n" +
 				"Process Name: test-request-mapping-live-hover");
 
 	}
+
+	@Test
+	public void testLiveHoverHintMethod() throws Exception {
+
+		File directory = new File(
+				ProjectsHarness.class.getResource("/test-projects/test-request-mapping-live-hover/").toURI());
+		String docUri = "file://" +directory.getAbsolutePath() + "/src/main/java/example/RestApi.java";
+
+
+		// Build a mock running boot app
+		mockAppProvider.builder()
+			.isSpringBootApp(true)
+			.containsLanguageServerProcessPropery(false)
+			.port("999")
+			.processId("76543")
+			.host("cfapps.io")
+			.processName("test-request-mapping-live-hover")
+			// Ugly, but this is real JSON copied from a real live running app. We want the
+			// mock app to return realistic results if possible
+			.getRequestMappings(
+				"{\"/webjars/**\":{\"bean\":\"resourceHandlerMapping\"},\"/**\":{\"bean\":\"resourceHandlerMapping\"},\"/**/favicon.ico\":{\"bean\":\"faviconHandlerMapping\"},\"{[/hello-world],methods=[GET]}\":{\"bean\":\"requestMappingHandlerMapping\",\"method\":\"public example.Greeting example.HelloWorldController.sayHello(java.lang.String)\"},\"{[/goodbye]}\":{\"bean\":\"requestMappingHandlerMapping\",\"method\":\"public java.lang.String example.RestApi.goodbye()\"},\"{[/hello]}\":{\"bean\":\"requestMappingHandlerMapping\",\"method\":\"public java.lang.String example.RestApi.hello()\"},\"{[/error]}\":{\"bean\":\"requestMappingHandlerMapping\",\"method\":\"public org.springframework.http.ResponseEntity<java.util.Map<java.lang.String, java.lang.Object>> org.springframework.boot.autoconfigure.web.BasicErrorController.error(javax.servlet.http.HttpServletRequest)\"},\"{[/error],produces=[text/html]}\":{\"bean\":\"requestMappingHandlerMapping\",\"method\":\"public org.springframework.web.servlet.ModelAndView org.springframework.boot.autoconfigure.web.BasicErrorController.errorHtml(javax.servlet.http.HttpServletRequest,javax.servlet.http.HttpServletResponse)\"}}")
+		.	build();
+
+		harness.intialize(directory);
+
+		Editor editor = harness.newEditorFromFileUri(docUri, LanguageId.JAVA);
+		editor.assertHoverContains("@RequestMapping(\"/hello\")", "Path: [/hello](http://cfapps.io:999/hello)\n" +
+				"\n" +
+				"Process ID: 76543\n" +
+				"\n" +
+				"Process Name: test-request-mapping-live-hover");
+
+		editor.assertHoverContains("@RequestMapping(\"/goodbye\")", "Path: [/goodbye](http://cfapps.io:999/goodbye)\n" +
+				"\n" +
+				"Process ID: 76543\n" +
+				"\n" +
+				"Process Name: test-request-mapping-live-hover");
+
+	}
+
 
 }
