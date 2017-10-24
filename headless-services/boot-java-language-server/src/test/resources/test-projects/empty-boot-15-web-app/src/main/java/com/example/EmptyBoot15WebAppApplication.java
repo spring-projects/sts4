b8aa5e484c52191531473ea6692baed5dac9909a
Kris De Volder <kdevolder@pivotal.io>
2017-10-24 10:37:09 -0700

    Add empty-boot-15-web-app test project

---
+++ headless-services/boot-java-language-server/src/test/resources/test-projects/empty-boot-15-web-app/src/main/java/com/example/EmptyBoot15WebAppApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyBoot15WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptyBoot15WebAppApplication.class, args);
+	}
+}
