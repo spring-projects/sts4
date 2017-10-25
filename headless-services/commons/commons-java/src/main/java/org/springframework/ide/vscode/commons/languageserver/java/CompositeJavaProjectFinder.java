23d55a6d93aec781af700249985f454a6fc98488
Kris De Volder <kdevolder@pivotal.io>
2017-10-25 10:56:11 -0700

    Cleanup imports

--- headless-services/commons/commons-java/src/main/java/org/springframework/ide/vscode/commons/languageserver/java/CompositeJavaProjectFinder.java
+++ headless-services/commons/commons-java/src/main/java/org/springframework/ide/vscode/commons/languageserver/java/CompositeJavaProjectFinder.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 import org.eclipse.lsp4j.TextDocumentIdentifier;
