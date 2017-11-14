9d1d36004a5ebe1bab90b8edac3d0501debe59fa
Kris De Volder <kdevolder@pivotal.io>
2017-11-14 10:29:00 -0800

    Fix failing test

---
+++ headless-services/commons/commons-boot-app-cli/src/test/resources/boot-apps/source-projects/actuator-client-20-test-subject/src/main/java/com/example/ActuatorClient20TestSubjectApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorClient20TestSubjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorClient20TestSubjectApplication.class, args);
+	}
+}
