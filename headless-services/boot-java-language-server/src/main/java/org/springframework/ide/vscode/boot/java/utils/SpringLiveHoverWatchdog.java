a95384ca5e3f6d0fb1ebb153a28d9d04e497ab62
Martin Lippert <mlippert@pivotal.io>
2018-01-09 10:32:23 +0100

    changed polling mechanics for liv hovers to reduce polling pressure in case something is slow in responding

--- headless-services/boot-java-language-server/src/main/java/org/springframework/ide/vscode/boot/java/utils/SpringLiveHoverWatchdog.java
+++ headless-services/boot-java-language-server/src/main/java/org/springframework/ide/vscode/boot/java/utils/SpringLiveHoverWatchdog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Pivotal, Inc.
+ * Copyright (c) 2017, 2018 Pivotal, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -109,7 +109,7 @@ public class SpringLiveHoverWatchdog {
 				}
 			};
 
-			timer.scheduleAtFixedRate(task, 0, POLLING_INTERVAL_MILLISECONDS);
+			timer.schedule(task, 0, POLLING_INTERVAL_MILLISECONDS);
 		}
 	}
 
