6f03e83e5921ece5580e6f1866aa69ac28a34d49
Kris De Volder <kdevolder@pivotal.io>
2018-02-08 12:05:54 -0800

    Try and fix some build issues temporarily.

---
+++ headless-services/boot-language-server/src/main/java/org/springframework/ide/vscode/boot/Main.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2017 Pivotal, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Pivotal, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.vscode.boot;
+
+import java.io.IOException;
+
+import org.springframework.ide.vscode.commons.languageserver.LaunguageServerApp;
+import org.springframework.ide.vscode.commons.util.LogRedirect;
+
+/**
+ * Starts up Language Server process
+ * 
+ * @author Alex Boyko
+ * @author Kris De Volder
+ *
+ */
+public class Main {
+		
+	public static void main(String[] args) throws IOException, InterruptedException {
+		String serverName = "boot-language-server";
+		LogRedirect.redirectToFile(serverName);
+		//TODO: wrap both BootProperties and BootJavaLanguageServers into a composite of some kind.
+		LaunguageServerApp.start(serverName,
+				() -> new BootPropertiesLanguageServer(BootPropertiesLanguageServerParams.createDefault()));
+	}
+
+}
