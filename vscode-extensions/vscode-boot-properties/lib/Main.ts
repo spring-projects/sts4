31b30efff41a9df4354d574d3cf60763fbf02eba
Kris De Volder <kdevolder@pivotal.io>
2018-02-21 11:23:04 -0800

    Fix problem building dummy vscode-boot-properties

--- vscode-extensions/vscode-boot-properties/lib/Main.ts
+++ vscode-extensions/vscode-boot-properties/lib/Main.ts
@@ -3,14 +3,6 @@
 // Import the module and reference it with the alias vscode in your code below
 
 import * as VSCode from 'vscode';
-import * as Path from 'path';
-import * as FS from 'fs';
-import * as Net from 'net';
-import * as ChildProcess from 'child_process';
-import {LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, StreamInfo} from 'vscode-languageclient';
-import {TextDocument} from 'vscode';
-
-import * as commons from 'commons-vscode';
 
 const PROPERTIES_LANGUAGE_ID = "spring-boot-properties";
 const YAML_LANGUAGE_ID = "spring-boot-properties-yaml";
