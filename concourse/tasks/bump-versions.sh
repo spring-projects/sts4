55c43d838e9ae0dd3357dc33e78e9769ab5489c1
BoykoAlex <aboyko@pivotal.io>
2018-02-22 18:19:06 -0500

    Fix syntax error in the script

--- concourse/tasks/bump-versions.sh
+++ concourse/tasks/bump-versions.sh
@@ -26,6 +26,7 @@ for extension_id in $(ls -d atom-*)
 do
     if [ $extension_id = "atom-commons" ]; then
         # skip commons package
+        echo "Skipping $extension_id"
     else
         cd $atom_sources/$extension_id
         echo "Should update version of $extension_id to $version"
