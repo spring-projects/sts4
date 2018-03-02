0ae7108bbee406d23fcb9f7a429aad893d5871e8
Kris De Volder <kdevolder@pivotal.io>
2018-03-01 22:36:46 -0800

    Fix compile error in vscode-bosh

--- vscode-extensions/vscode-bosh/lib/Main.ts
+++ vscode-extensions/vscode-bosh/lib/Main.ts
@@ -31,7 +31,6 @@ export function activate(context: VSCode.ExtensionContext) {
         DEBUG : false,
         CONNECT_TO_LS: false,
         extensionId: 'vscode-bosh',
-        launcher: (context: VSCode.ExtensionContext) => Path.resolve(context.extensionPath, 'jars/language-server.jar'),
         jvmHeap: "48m",
         clientOptions: {
             documentSelector: [ 
