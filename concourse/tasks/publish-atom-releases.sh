f047d29dade49202fc358b7c057f2b49c8ed8f61
BoykoAlex <aboyko@pivotal.io>
2018-03-09 15:49:12 -0500

    More corrections to Atom publish scripts

--- concourse/tasks/publish-atom-releases.sh
+++ concourse/tasks/publish-atom-releases.sh
@@ -2,18 +2,13 @@
 set -e
 workdir=`pwd`
 
-atom_packages=`ls ${workdir}/atom-*`
+atom_packages=`ls -d ${workdir}/atom-*`
 
 for atom_package in $atom_packages
 do
     echo "****************************************************************"
     echo "*** Publishing : ${atom_package}"
     echo "****************************************************************"
-    echo ""
-    echo "We are runing the following command:"
-    echo ""
-    echo "     apm publish -p vsce_token --packagePath $vsix_file"
-    echo ""
     cd $workdir/$atom_package
     tag=v$(cat package.json | jq -r ".version")
     apm login --token $atom_token
