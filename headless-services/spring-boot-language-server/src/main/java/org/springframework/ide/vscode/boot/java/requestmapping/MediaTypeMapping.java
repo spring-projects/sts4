1adbab0ac0f71281ca5accd754e0938da6e716fe
Martin Lippert <mlippert@pivotal.io>
2018-03-09 11:40:01 +0100

    added additional media types, taken from spring 5 MediaType constants

--- headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/requestmapping/MediaTypeMapping.java
+++ headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/requestmapping/MediaTypeMapping.java
@@ -15,11 +15,30 @@ package org.springframework.ide.vscode.boot.java.requestmapping;
  */
 public enum MediaTypeMapping {
 	
-	TEXT_PLAIN("text/plain"),
+	ALL("*/*"),
+	APPLICATION_ATOM_XML("application/atom+xml"),
+	APPLICATION_FORM_URLENCODED("application/x-www-form-urlencoded"),
 	APPLICATION_JSON("application/json"),
-	APPLICATION_STREAM_JSON("application/stream+json");
+	APPLICATION_JSON_UTF8("application/json;charset=UTF-8"),
+	APPLICATION_OCTET_STREAM("application/octet-stream"),
+	APPLICATION_PDF("application/pdf"),
+	APPLICATION_PROBLEM_JSON("application/problem+json"),
+	APPLICATION_PROBLEM_JSON_UTF8("application/problem+json;charset=UTF-8"),
+	APPLICATION_PROBLEM_XML("application/problem+xml"),
+	APPLICATION_RSS_XML("application/rss+xml"),
+	APPLICATION_STREAM_JSON("application/stream+json"),
+	APPLICATION_XHTML_XML("application/xhtml+xml"),
+	APPLICATION_XML("application/xml"),
+	IMAGE_GIF("image/gif"),
+	IMAGE_JPEG("image/jpeg"),
+	IMAGE_PNG("image/png"),
+	MULTIPART_FORM_DATA("multipart/form-data"),
+	TEXT_EVENT_STREAM("text/event-stream"),
+	TEXT_HTML("text/html"),
+	TEXT_MARKDOWN("text/markdown"),
+	TEXT_PLAIN("text/plain"),
+	TEXT_XML("text/xml");
 
-	
 	private String mediaType;
 
 	private MediaTypeMapping(String mediaType) {
