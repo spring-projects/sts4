90e52ab6e001d06774eefa816e3d3658d9d80465
Kris De Volder <kdevolder@pivotal.io>
2018-03-20 14:56:33 -0700

    Update build.sh to require less changes in jdt.ls and vscode-java

--- vscode-extensions/vscode-spring-boot/build.sh
+++ vscode-extensions/vscode-spring-boot/build.sh
@@ -6,7 +6,7 @@ workdir=$(pwd)
 # Build jdt.ls
 cd ${HOME}/git/eclipse.jdt.ls
 jdt_ls_repo=$(pwd)
-./mvnw -Pserver-distro clean package
+./mvnw -Pserver-distro -Pupdate-site clean package
 
 # Build vscode-java
 cd ${HOME}/git/vscode-java
@@ -15,7 +15,8 @@ mkdir server
 tar -xvzf ${jdt_ls_repo}/org.eclipse.jdt.ls.product/distro/jdt-language-server-*.tar.gz -C ./server
 npm install
 rm -fr *.vsix
-npm run vsce-package
+npm install -D vsce
+./node_modules/.bin/vsce package
 code --uninstall-extension redhat.java || echo "Not installed redhat.java"
 code --install-extension *.vsix
 
