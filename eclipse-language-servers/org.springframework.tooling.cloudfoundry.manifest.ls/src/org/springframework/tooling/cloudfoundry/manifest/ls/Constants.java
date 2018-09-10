391d73de05c9ed1d500877a5597b8ba4ffdf27be
Martin Lippert <mlippert@pivotal.io>
2018-09-10 11:05:24 +0200

    updated versions of eclipse ls bundles to 0.7.0

--- eclipse-language-servers/org.springframework.tooling.cloudfoundry.manifest.ls/src/org/springframework/tooling/cloudfoundry/manifest/ls/Constants.java
+++ eclipse-language-servers/org.springframework.tooling.cloudfoundry.manifest.ls/src/org/springframework/tooling/cloudfoundry/manifest/ls/Constants.java
@@ -16,6 +16,6 @@ package org.springframework.tooling.cloudfoundry.manifest.ls;
 public class Constants {
 
 	public static final String PLUGIN_ID = "org.springframework.tooling.cloudfoundry.manifest.ls";
-	public static final String LANGUAGE_SERVER_VERSION = "0.6.0-SNAPSHOT.jar";
+	public static final String LANGUAGE_SERVER_VERSION = "0.7.0-SNAPSHOT.jar";
 
 }
