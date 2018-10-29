a24f697ce11441a97c0a2cec0894fc69e63c26d8
BoykoAlex <aboyko@pivotal.io>
2018-10-29 19:13:03 -0400

    v0.0.5-alpha

--- theia-extensions/theia-spring-boot/spring-boot/src/browser/monaco-properties-contribution.ts
+++ theia-extensions/theia-spring-boot/spring-boot/src/browser/monaco-properties-contribution.ts
@@ -1,5 +1,5 @@
-/// <reference types='@typefox/monaco-editor-core/monaco'/>
 import {
+    JAVA_PROPERTIES_LANGUAGE_ID,
     BOOT_PROPERTIES_LANGUAGE_ID,
     BOOT_PROPERTIES_LANGUAGE_NAME,
 } from '../common';
@@ -16,7 +16,9 @@ monaco.languages.register({
 
 monaco.languages.onLanguage(BOOT_PROPERTIES_LANGUAGE_ID, () => {
     if (!PROPERTIES_LANG_MODULE_PROMISE) {
-        PROPERTIES_LANG_MODULE_PROMISE = (<any>monaco.languages.getLanguages().find(ext => ext.id === 'ini')).loader();
+        const languages = monaco.languages.getLanguages();
+        console.log('All languages: ' + JSON.stringify(languages.map(l => l.id)));
+        PROPERTIES_LANG_MODULE_PROMISE = (<any>languages.find(ext => ext.id === JAVA_PROPERTIES_LANGUAGE_ID)).loader();
     }
     return PROPERTIES_LANG_MODULE_PROMISE.then(mod => {
         monaco.languages.setLanguageConfiguration(BOOT_PROPERTIES_LANGUAGE_ID, mod.conf);
