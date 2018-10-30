574091ba5a181c4a104c5122b4f9b1e6b3b777c4
BoykoAlex <aboyko@pivotal.io>
2018-10-30 12:14:32 -0400

    v0.0.5-gamma

---
+++ theia-extensions/theia-commons/src/browser/java-properties-grammar-contribution.ts
@@ -0,0 +1,36 @@
+import { JAVA_PROPERTIES_LANGUAGE_ID, JAVA_PROPERTIES_LANGUAGE_GRAMMAR_SCOPE } from '../common';
+import { LanguageGrammarDefinitionContribution, TextmateRegistry } from '@theia/monaco/lib/browser/textmate';
+import { injectable } from 'inversify';
+import { JAVA_PROPERTIES_CONFIG, JAVA_PROPERTIES_TM_GRAMMAR } from '@pivotal-tools/theia-languageclient/lib/browser/languages';
+
+// Java .properties file language registration
+
+@injectable()
+export class JavaPropertiesGrammarContribution implements LanguageGrammarDefinitionContribution {
+
+    registerTextmateLanguage(registry: TextmateRegistry) {
+        monaco.languages.register({
+            id: JAVA_PROPERTIES_LANGUAGE_ID,
+            aliases: [
+                "Java Properties"
+            ],
+            extensions: [
+                ".properties"
+            ],
+            filenames: []
+        });
+
+        monaco.languages.setLanguageConfiguration(JAVA_PROPERTIES_LANGUAGE_ID, JAVA_PROPERTIES_CONFIG);
+
+        registry.registerTextmateGrammarScope(JAVA_PROPERTIES_LANGUAGE_GRAMMAR_SCOPE, {
+            async getGrammarDefinition() {
+                return {
+                    format: 'json',
+                    content: JAVA_PROPERTIES_TM_GRAMMAR
+                };
+            }
+        });
+
+        registry.mapLanguageIdToTextmateGrammar(JAVA_PROPERTIES_LANGUAGE_ID, JAVA_PROPERTIES_LANGUAGE_GRAMMAR_SCOPE);
+    }
+}
