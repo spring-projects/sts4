6ddbe4b7a92c24c61f9550c13f8fb9f8022ec844
Kris De Volder <kdevolder@pivotal.io>
2018-11-06 09:25:21 -0800

    Bump ls version to 1.1.1 eclipsy bits.

--- eclipse-language-servers/org.springframework.tooling.concourse.ls/src/org/springframework/tooling/concourse/ls/Constants.java
+++ eclipse-language-servers/org.springframework.tooling.concourse.ls/src/org/springframework/tooling/concourse/ls/Constants.java
@@ -16,6 +16,6 @@ package org.springframework.tooling.concourse.ls;
 public class Constants {
 	
 	public static final String PLUGIN_ID = "org.springframework.tooling.concourse.ls";
-	public static final String LANGUAGE_SERVER_VERSION = "1.1.0-SNAPSHOT";
+	public static final String LANGUAGE_SERVER_VERSION = "1.1.1-SNAPSHOT";
 
 }
