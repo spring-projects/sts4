29228edc93bfefa33702934529fb04e33483327c
Kris De Volder <kdevolder@pivotal.io>
2019-02-25 12:04:31 -0800

    Some improvements to jdt-ls-extensions building

--- headless-services/spring-boot-language-server/build.sh
+++ headless-services/spring-boot-language-server/build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -e
-../mvnw \
-    -f ../pom.xml \
-    -pl spring-boot-language-server \
-    -am \
-    clean install
+modules=spring-boot-language-server,:org.springframework.tooling.jdt.ls.extension,:org.springframework.tooling.jdt.ls.commons.test
+cd ../jdt-ls-extension
+if command -v xvfb-run ; then
+    echo "Using xvfb to run in headless environment..."
+    xvfb-run ../mvnw \
+        -f ../pom.xml \
+        -pl $modules \
+        -am \
+        clean install
+else
+    ../mvnw \
+        -f ../pom.xml \
+        -pl $modules \
+        -am \
+        clean install
+fi
