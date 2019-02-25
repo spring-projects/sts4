21123883f97133538f1e03ae94970f734820fb74
Kris De Volder <kdevolder@pivotal.io>
2019-02-25 09:18:08 -0800

    Revert "Remove workaround for  https://github.com/spring-projects/sts4/issues/193"
    This reverts commit 3faacd88ffb6dd2044758c03ec43a0f860dab010.

--- headless-services/commons/language-server-starter/src/main/java/org/springframework/ide/vscode/languageserver/starter/LanguageServerAutoConf.java
+++ headless-services/commons/language-server-starter/src/main/java/org/springframework/ide/vscode/languageserver/starter/LanguageServerAutoConf.java
@@ -50,7 +50,10 @@ public class LanguageServerAutoConf {
 			Optional<CompletionFilter> completionFilter
 	) throws Exception {
 		SimpleLanguageServer server = new SimpleLanguageServer(props.getExtensionId());
-		server.setCompletionTriggerCharacters(props.getCompletionTriggerCharacters());
+		if (LspClient.currentClient()!=Client.VSCODE) {
+			//Vscode excluded. See https://github.com/spring-projects/sts4/issues/193
+			server.setCompletionTriggerCharacters(props.getCompletionTriggerCharacters());
+		}
 		server.setCompletionFilter(completionFilter);
 		severities.ifPresent(server::setDiagnosticSeverityProvider);
 		return server;
