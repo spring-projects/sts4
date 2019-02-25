b65b521c67a2b4815751d50ac8cf6dea9412d33c
Kris De Volder <kdevolder@pivotal.io>
2019-02-25 12:14:31 -0800

    Try to fix vscode-spring-boot ci build

--- headless-services/spring-boot-language-server/build.sh
+++ headless-services/spring-boot-language-server/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-modules=spring-boot-language-server,:org.springframework.tooling.jdt.ls.extension,:org.springframework.tooling.jdt.ls.commons.test
+modules=spring-boot-language-server,:org.springframework.tooling.jdt.ls.extension,:org.springframework.tooling.jdt.ls.commons,:org.springframework.tooling.jdt.ls.commons.test
 cd ../jdt-ls-extension
 if command -v xvfb-run ; then
     echo "Using xvfb to run in headless environment..."
