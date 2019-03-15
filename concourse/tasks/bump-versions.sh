7c84605075cbe1e32cafb3ead0af4be3b7009c35
BoykoAlex <aboyko@pivotal.io>
2019-03-15 11:42:14 -0400

    PT #164169543: Adjust permissions for tasks files

--- concourse/tasks/bump-versions.sh
+++ concourse/tasks/bump-versions.sh
@@ -5,6 +5,7 @@ workdir=`pwd`
 
 vscode_sources=$workdir/sts4/vscode-extensions
 atom_sources=$workdir/sts4/atom-extensions
+theia_sources=$workdir/sts4/theia-extensions
 
 version=`cat version/version`
 echo "version=$version"
@@ -26,6 +27,20 @@ for extension_id in $(ls -d atom-*)
 do
     if [ $extension_id != "atom-commons" ]; then
         cd $atom_sources/$extension_id
+        echo "Should update version of $extension_id to $version"
+        npm version $version
+        git add package.json
+        echo ""
+    fi
+done
+
+# theia extensions
+theia_version=cat version/version`
+echo "theia-version=$theia_version"
+for extension_id in $(ls -d theia-*)
+do
+    if [ $extension_id != "theia-commons" ]; then
+        cd "${theia_sources}/theia-$extension_id"
         echo "Should update version of $extension_id to $version"
         npm version $version
         git add package.json
