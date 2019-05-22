5bec2412723d8b273cf60a18bf260dc15ebba548
Kris De Volder <kdevolder@pivotal.io>
2019-05-22 13:23:05 -0700

    Convert boot-ls application.yml to .properties again

--- vscode-extensions/vscode-spring-boot/lib/Main.ts
+++ vscode-extensions/vscode-spring-boot/lib/Main.ts
@@ -29,7 +29,7 @@ export function activate(context: VSCode.ExtensionContext): Thenable<LanguageCli
         explodedLsJarData: {
             lsLocation: 'language-server',
             mainClass: 'org.springframework.ide.vscode.boot.app.BootLanguagServerBootApp',
-            configFileName: 'application.yml'
+            configFileName: 'application.properties'
         },
         workspaceOptions: VSCode.workspace.getConfiguration("spring-boot.ls"),
         clientOptions: {
