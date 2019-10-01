b2c7ab2fd53d8cfaec283eed891ce9f943dd4f63
BoykoAlex <aboyko@pivotal.io>
2019-10-01 13:33:05 -0400

    Concourse job to mirror repo for vscode-spring-boot

---
+++ concourse/tasks/sync-mirror-repo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+set -v
+workdir=`pwd`
+output=$workdir/out
+
+cd sts4
+origin_url="$(git config --get remote.origin.url)"
+
+cd $output
+
+echo "Syncing original repo $origin_url' and mirror repo '$mirror_repo'"
+
+git config user.email "aboyko@pivotal.io"
+git config user.name "Alex Boyko"
+
+git clone --mirror $origin_url original-repo
+cd original-repo
+git remote add sync $mirror_repo
+git push sync --all
