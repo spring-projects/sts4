00cfbc8b3b6dae07283688f54ecb1b8d105ab648
Kris De Volder <kdevolder@pivotal.io>
2019-11-25 15:44:52 -0800

    More complete Theia docker image

--- theia-extensions/k8s-app/docker-build-and-run.sh
+++ theia-extensions/k8s-app/docker-build-and-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+JAVA_PROJECT_DIR=/home/kdvolder/git/kdvolder/chatter
+
 docker build -t kdvolder/k8s-theia .
-docker run -it -p 3000:3000 kdvolder/k8s-theia
+docker run -it --init  -p 3000:3000 -v "${JAVA_PROJECT_DIR}:/home/project" kdvolder/k8s-theia
