3b91703f19380acea0f1b219e09d727ebdb04d14
Kris De Volder <kdevolder@pivotal.io>
2019-11-27 14:48:26 -0800

    Fix some issues with theia-docker-image build

--- concourse/tasks/prepare-theia-docker-image.sh
+++ concourse/tasks/prepare-theia-docker-image.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 workdir=$(pwd)
+
+echo "workdir=${workdir}"
+
 cp -R ${workdir}/sts4/concourse/theia-docker-image/* ${workdir}/output
 for i in ${workdir}/*-vsix-*/*.vsix ; do
+    echo "Adding plugin: $i"
     cp "$i" ${workdir}/output/plugins/
 done
 
