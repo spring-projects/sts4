f16513c8031cd5e7a6047b1f759f753a91852461
Kris De Volder <kdevolder@pivotal.io>
2020-01-10 11:16:00 -0800

    First attempt at 'create-release-wiki-page' script

--- concourse/tasks/create-release-wiki-page.sh
+++ concourse/tasks/create-release-wiki-page.sh
@@ -10,37 +10,26 @@ set -e
 set -v
 workdir=`pwd`
 
-vsix_files=$(find *-vsix -name "*.vsix") 
+vsix_files=$(find "$workdir" *-vsix -name "*.vsix") 
 # version=`cat version/version`
 # echo "version=$version"
 
-# vscode extensions
-wikidir=${workdir}/sts4
-cd $vscode_sources
-for extension_id in $(ls -d vscode-*)
+
+page=$workdir/sts4-wiki/VSCode-Extensions-Downloads-Latest.md
+
+echo "Current Vscode Extensions Downloads" > $page
+echo "===================================" >> $page
+echo "" >> $page
+for vsix_file in ${vsix_files}
 do
-    cd $vscode_sources/$extension_id
-    echo "Should update version of $extension_id to $version"
-    npm version $version
-    git add package.json
-    echo ""
+    fname=$(basename $vsix_files)
+    echo "- [$fname](https://dist.springsource.com/release/STS4/vscode/$fname)" >> $page
 done
 
-# atom extensions
-cd $atom_sources
-for extension_id in $(ls -d atom-*)
-do
-    if [ $extension_id != "atom-commons" ]; then
-        cd $atom_sources/$extension_id
-        echo "Should update version of $extension_id to $version"
-        npm version $version
-        git add package.json
-        echo ""
-    fi
-done
-
-cd $workdir/sts4/headless-services
-$workdir/sts4/concourse/tasks/update-pom-versions.sh $version
+echo "Vsix Downloads Page Generated:"
+echo "----------------------------------------------"
+cat $page
+echo "----------------------------------------------"
 
 git config user.email "kdevolder@pivotal.io"
 git config user.name "Kris De Volder"
@@ -48,6 +37,6 @@ git config user.name "Kris De Volder"
 git add .
 
 git commit \
-    -m "Update vscode release wiki page"
+    -m "Update vscode latest downloads wiki page"
 
 git clone $workdir/sts4-wiki $workdir/sts4-wiki-out
