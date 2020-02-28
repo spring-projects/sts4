789a6c0e2689cfa4d4089fbbf32ae4334124dc0f
Kris De Volder <kdevolder@pivotal.io>
2020-02-28 11:34:13 -0800

    Cleanup obselete commented script code

--- concourse/tasks/prepare-theia-docker-image.sh
+++ concourse/tasks/prepare-theia-docker-image.sh
@@ -10,9 +10,5 @@ for i in ${workdir}/*-vsix-*/*.vsix ; do
     cp "$i" ${workdir}/output/plugins/
 done
 
-# Download vscode-java 0.53.1. Higher versions require vscode-client 0.40 or higher not available in Theia
-#vscode_java_url="https://marketplace.visualstudio.com/_apis/public/gallery/publishers/redhat/vsextensions/java/0.53.1/vspackage"
-#curl ${vscode_java_url} > ${workdir}/output/plugins/vscode-java.vsix
-
 ls -Rl ${workdir}/output
 
