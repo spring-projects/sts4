38b75b7ae9b7881f9f8db97eeefe2b3352af1a11
Kris De Volder <kdevolder@pivotal.io>
2020-03-31 12:25:26 -0700

    Add ovsx publishing to pipeline

---
+++ concourse/tasks/publish-ovsx-releases.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+workdir=`pwd`
+
+#Uncomments the below to publish all vsix files in the task inputs:
+#vsix_files=`ls ${workdir}/s3-*/*.vsix`
+
+#Uncomment the below to publish only concourse vxix
+#vsix_files=`ls ${workdir}/s3-*/vscode-concourse-*.vsix`
+
+#Uncomment the below to publish all vsix files
+vsix_files=`ls ${workdir}/s3-*/vscode-*.vsix`
+
+for vsix_file in $vsix_files
+do
+    echo "****************************************************************"
+    echo "*** Publishing : ${vsix_file} to https://open-vsx.org/"
+    echo "****************************************************************"
+    echo ""
+    echo "We are runing the following command:"
+    echo ""
+    echo "     vsce publish -p $vsce_token $vsix_file"
+    echo ""
+    #vsce publish -p $vsce_token $vsix_file
+done
