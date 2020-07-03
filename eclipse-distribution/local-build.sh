297fd044715baf10c013dbf7f30cf792f165fa2a
Kris De Volder <kdevolder@pivotal.io>
2020-07-03 11:31:09 -0700

    Helper script to build eclipse distro locally

---
+++ eclipse-distribution/local-build.sh
@@ -0,0 +1,1 @@
+mvn clean package -Pe415 -Psnapshot -Dsigning.skip=true -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5 -Dskip.osx.signing=true -Dskip.win.signing=true -Dskip.osx.notarizing=true
