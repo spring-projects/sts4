10f30d757ed616efea2cc3f249b5c215f2335027
Kris De Volder <kdevolder@pivotal.io>
2020-09-17 15:28:10 -0700

    Update local build because it broke
    Signed-off-by: Kris De Volder <kdevolder@pivotal.io>

--- eclipse-language-servers/local-build.sh
+++ eclipse-language-servers/local-build.sh
@@ -5,4 +5,4 @@ cd ../headless-services
 ./mvnw clean install -Dmaven.test.skip=true
 
 cd $workdir
-./mvnw -Pe415 clean install -Dmaven.test.skip=true
+./mvnw -Pe416 clean install -Dmaven.test.skip=true
