ac41eae0ebde8dc921b41ae980a137caaf3b4947
Kris De Volder <kdevolder@pivotal.io>
2020-10-20 12:35:05 -0700

    Fix boot-dev-pack-ovsx publish

--- concourse/tasks/ovsx-build-and-publish.sh
+++ concourse/tasks/ovsx-build-and-publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-workdir=`pwd`
-sources=$workdir/sts4/vscode-extensions/boot-dev-pack
+cd sts4/vscode-extensions/boot-dev-pack
+echo "Invoking ovsx publish in dir $(pwd)"
 ovsx publish -p $ovsx_token
