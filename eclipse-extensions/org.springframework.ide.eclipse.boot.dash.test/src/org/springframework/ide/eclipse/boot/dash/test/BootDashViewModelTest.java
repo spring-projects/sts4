eca0d6344aa0ff140fb2486f15e205a686deccbe
BoykoAlex <aboyko@pivotal.io>
2021-05-14 16:35:13 -0400

    Fix compiler error

--- eclipse-extensions/org.springframework.ide.eclipse.boot.dash.test/src/org/springframework/ide/eclipse/boot/dash/test/BootDashViewModelTest.java
+++ eclipse-extensions/org.springframework.ide.eclipse.boot.dash.test/src/org/springframework/ide/eclipse/boot/dash/test/BootDashViewModelTest.java
@@ -431,7 +431,7 @@ public class BootDashViewModelTest {
 		assertTrue(toggleFilters.getValue().isEmpty());
 		assertFilterAccepts(true, filter, "a-tag");
 
-		FilterChoice toggleFilter = new FilterChoice("foo", "Foo filter", new Filter<BootDashElement>() {
+		FilterChoice toggleFilter = new FilterChoice("foo", "Foo filter", "description", new Filter<BootDashElement>() {
 			public boolean accept(BootDashElement t) {
 				return t.getTags().contains("foo");
 			}
