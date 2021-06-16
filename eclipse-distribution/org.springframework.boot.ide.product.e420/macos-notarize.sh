6623ff31af471567185ac2e65e64ec0eb5118cff
Martin Lippert <lippertm@vmware.com>
2021-06-16 13:49:25 +0200

    adding more debug output to notarization script

--- eclipse-distribution/org.springframework.boot.ide.product.e420/macos-notarize.sh
+++ eclipse-distribution/org.springframework.boot.ide.product.e420/macos-notarize.sh
@@ -16,11 +16,22 @@ UUID=$(echo ${RESPONSE} | jq -r '.uuid')
 
 STATUS=$(echo ${RESPONSE} | jq -r '.notarizationStatus.status')
 
+echo "Status after submitting"
+echo ${STATUS}
+
 while [[ ${STATUS} == 'IN_PROGRESS' ]]; do
   sleep 1m
   RESPONSE=$(curl -s ${NOTARIZE_SERVICE_URL}/macos-notarization-service/${UUID}/status)
   STATUS=$(echo ${RESPONSE} | jq -r '.notarizationStatus.status')
+
+  echo "Waiting for status to change"
+  echo ${RESPONSE}
+  echo ${STATUS}
 done
+
+echo "Status is not in progress anymore"
+echo ${RESPONSE}
+echo ${STATUS}
 
 if [[ ${STATUS} != 'COMPLETE' ]]; then
   echo "Notarization failed: ${RESPONSE}"
