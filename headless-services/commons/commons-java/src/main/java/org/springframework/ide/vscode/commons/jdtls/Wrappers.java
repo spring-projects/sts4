8490b7b38efb137df9a0b316f8ac92a74899bd40
Martin Lippert <lippertm@vmware.com>
2021-11-29 16:02:22 +0100

    GH-690: fix NPE in type wrapper that caused property indexing and content-assist problems

--- headless-services/commons/commons-java/src/main/java/org/springframework/ide/vscode/commons/jdtls/Wrappers.java
+++ headless-services/commons/commons-java/src/main/java/org/springframework/ide/vscode/commons/jdtls/Wrappers.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Pivotal, Inc.
+ * Copyright (c) 2019, 2021 Pivotal, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -105,7 +105,7 @@ public class Wrappers {
 				@Override
 				public int dimensions() {
 					if (data.getExtras() != null && data.getExtras().containsKey("dimensions")) {
-						return (Integer) data.getExtras().get("dimenions");
+						return (Integer) data.getExtras().get("dimensions");
 					}
 					return 0;
 				}
