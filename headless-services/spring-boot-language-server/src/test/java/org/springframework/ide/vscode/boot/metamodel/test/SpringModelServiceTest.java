3387a91cfb1a8b4968930f02324eca9ac32a8551
Martin Lippert <lippertm@vmware.com>
2023-04-18 17:47:25 +0200

    service method to return beans as future waits for project-related indexing tasks to complete

---
+++ headless-services/spring-boot-language-server/src/test/java/org/springframework/ide/vscode/boot/metamodel/test/SpringModelServiceTest.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright (c) 2023 VMware, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     VMware, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.vscode.boot.metamodel.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.lsp4j.Location;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.Range;
+import org.eclipse.lsp4j.TextDocumentIdentifier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.ide.vscode.boot.app.SpringSymbolIndex;
+import org.springframework.ide.vscode.boot.bootiful.BootLanguageServerTest;
+import org.springframework.ide.vscode.boot.bootiful.SymbolProviderTestConf;
+import org.springframework.ide.vscode.boot.index.SpringMetamodelIndex;
+import org.springframework.ide.vscode.commons.languageserver.java.JavaProjectFinder;
+import org.springframework.ide.vscode.commons.protocol.spring.Bean;
+import org.springframework.ide.vscode.commons.protocol.spring.InjectionPoint;
+import org.springframework.ide.vscode.project.harness.BootLanguageServerHarness;
+import org.springframework.ide.vscode.project.harness.ProjectsHarness;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * @author Martin Lippert
+ */
+@ExtendWith(SpringExtension.class)
+@BootLanguageServerTest
+@Import(SymbolProviderTestConf.class)
+public class SpringModelServiceTest {
+
+	@Autowired private BootLanguageServerHarness harness;
+	@Autowired private JavaProjectFinder projectFinder;
+	@Autowired private SpringSymbolIndex indexer;
+
+	private File directory;
+
+	@BeforeEach
+	public void setup() throws Exception {
+		harness.intialize(null);
+
+		directory = new File(ProjectsHarness.class.getResource("/test-projects/test-spring-indexing/").toURI());
+
+		String projectDir = directory.toURI().toString();
+
+		// trigger project creation
+		projectFinder.find(new TextDocumentIdentifier(projectDir)).get();
+
+//		CompletableFuture<Void> initProject = indexer.waitOperation();
+//		initProject.get(5, TimeUnit.SECONDS);
+	}
+
+	@Test
+	void testSpringModelServiceBeansForNonExistingProject() throws Exception {
+		CompletableFuture<List<Bean>> result = indexer.beans("random-project");
+
+		List<Bean> list = result.get();
+		assertNull(list);
+	}
+
+	@Test
+	void testBeansNameAndTypeFromBeanAnnotatedMethod() throws Exception {
+		CompletableFuture<List<Bean>> result = indexer.beans("test-spring-indexing");
+
+		List<Bean> beans = result.get(5, TimeUnit.SECONDS);
+
+		assertNotNull(beans);
+		assertEquals(11, beans.size());
+	}
+
+}
