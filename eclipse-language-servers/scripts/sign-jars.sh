5be4cd3a11147a879d07ae5351993ace87fd1236
aboyko <aboyko@vmware.com>
2023-06-09 16:43:37 -0400

    Sign script POC

---
+++ eclipse-language-servers/scripts/sign-jars.sh
@@ -0,0 +1,20 @@
+set -e
+if [ -d "$1" ]
+then
+  echo "Sign JARs in directory: $1"
+  files=`ls $1`
+  for file in $files
+  do
+    if ! [ -d $file ] && [[ $file == *.jar ]]
+    then
+      echo "Copying $file to remote machine..."
+      scp -i $SSH_KEY $1/$file $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo
+      echo "Signing $file..."
+      echo "Copying **signed** $file back... (into $1/$file)"
+      scp -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo/$file $1/$file
+      echo "Cleaning up remote machine..."
+      ssh -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com -- rm /opt/bamboo/$file
+      echo "Successfully signed $file"
+    fi
+  done
+fi
