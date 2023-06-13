f56fcf3cdbee8de6396afad12a376589b5a8bf71
aboyko <aboyko@vmware.com>
2023-06-12 20:52:20 -0400

    Sign script simplification and logging for debugging

--- eclipse-language-servers/scripts/sign-jars.sh
+++ eclipse-language-servers/scripts/sign-jars.sh
@@ -5,16 +5,23 @@ then
   files=`ls $1`
   for file in $files
   do
-    if ! [ -d $file ] && [ "${file: -4}" == ".jar" ]
+    echo "Found $file..."
+    if ! [ -d $file ]
     then
-      echo "Copying $file to remote machine..."
-      scp -i $SSH_KEY $1/$file $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo
-      echo "Signing $file..."
-      echo "Copying **signed** $file back... (into $1/$file)"
-      scp -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo/$file $1/$file
-      echo "Cleaning up remote machine..."
-      ssh -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com -- rm /opt/bamboo/$file
-      echo "Successfully signed $file"
+      echo "Looking at: $1/$file"
+      extension="${file##*.}"
+      echo "Detected extension = $extension"
+      if [ $extension == "jar" ]
+      then
+        echo "Copying $file to remote machine..."
+        scp -i $SSH_KEY $1/$file $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo
+        echo "Signing $file..."
+        echo "Copying **signed** $file back... (into $1/$file)"
+        scp -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo/$file $1/$file
+        echo "Cleaning up remote machine..."
+        ssh -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com -- rm /opt/bamboo/$file
+        echo "Successfully signed $file"
+      fi
     fi
   done
 fi
