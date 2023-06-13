b687eb8f5bad5d5c06cecdd038ad564f9d590ed5
aboyko <aboyko@vmware.com>
2023-06-13 14:08:04 -0400

    Add jar sign command to the script

--- eclipse-language-servers/scripts/sign-jars.sh
+++ eclipse-language-servers/scripts/sign-jars.sh
@@ -5,19 +5,20 @@ then
   files=`ls $1`
   for file in $files
   do
-    echo "Found $file..."
+#    echo "Found $file..."
     if ! [ -d $file ]
     then
-      echo "Looking at: $1/$file"
+#      echo "Looking at: $1/$file"
       extension="${file##*.}"
-      echo "Detected extension = $extension"
+#      echo "Detected extension = $extension"
       if [ "$extension" = "jar" ]
       then
         echo "Copying $file to remote machine..."
         scp -i $SSH_KEY $1/$file $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo
         echo "Signing $file..."
+        ssh -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com -- /build/apps/signing/signserver/signc -v --input=/opt/bamboo/$file --keyid=jarsign_vmware --signmethod="jdk-1.8.0_121" --output=/opt/bamboo/signed-$file
         echo "Copying **signed** $file back... (into $1/$file)"
-        scp -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo/$file $1/$file
+        scp -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo/signed-$file $1/$file
         echo "Cleaning up remote machine..."
         ssh -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com -- rm /opt/bamboo/$file
         echo "Successfully signed $file"
