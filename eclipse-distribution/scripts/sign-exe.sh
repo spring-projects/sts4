52b4faad4aba1242811f17d03a408e40a15d4d40
aboyko <aboyko@vmware.com>
2023-06-14 13:55:54 -0400

    Cleanup jar and exe signing

--- eclipse-distribution/scripts/sign-exe.sh
+++ eclipse-distribution/scripts/sign-exe.sh
@@ -4,7 +4,6 @@ out_file=$2
 
 in_filename="$(basename -- $in_file)"
 echo "Copying $in_filename to remote machine..."
-echo "Input file: $in_file"
 scp -i $SSH_KEY $in_file $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo
 echo "Signing $in_filename..."
 ssh -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com -- /build/apps/signing/signserver/signc -v --input=/opt/bamboo/$in_filename --keyid=authenticode_SHA2 --signmethod="winddk-8.1" --output=/opt/bamboo/$in_filename --hash sha256
