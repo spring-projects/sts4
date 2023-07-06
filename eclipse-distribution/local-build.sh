d7b46b48b8233d571d6d4dcff45d69f659a46156
aboyko <aboyko@vmware.com>
2023-07-06 13:55:15 -0400

    Eclipse distro add maven wrapper and use it

--- eclipse-distribution/local-build.sh
+++ eclipse-distribution/local-build.sh
@@ -1,2 +1,2 @@
-mvn clean package -Pe428 -Psnapshot -Dsigning.skip=true -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5 -Dskip.osx.signing=true -Dskip.win.signing=true -Dskip.osx.notarizing=true
+./mvnw clean package -Pe428 -Psnapshot -Dsigning.skip=true -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5 -Dskip.osx.signing=true -Dskip.win.signing=true -Dskip.osx.notarizing=true
 
