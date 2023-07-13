da09c54c0238dccf5fcdb1c0288da568447ca8c3
aboyko <aboyko@vmware.com>
2023-07-13 18:35:49 -0400

    GH Hosted: exe signing corrections

--- .github/scripts/sign-exe-in-zip-file.sh
+++ .github/scripts/sign-exe-in-zip-file.sh
@@ -1,6 +1,7 @@
 file=$1
-sign_script=$2
 filename="$(basename -- $file)"
+
+workdir=`pwd`
 
 echo "****************************************************************"
 echo "*** Processing : ${file}"
@@ -9,5 +10,7 @@ destination_folder_name=extracted_${filename}
 unzip -q $file -d ./${destination_folder_name}
 sts_folder=`find ./${destination_folder_name} -maxdepth 1 -type d -name 'sts-*' -print -quit`
 echo "Found folder: ${sts_folder}"
-$sign_script ./${sts_folder}/SpringToolSuite4.exe ${sts_folder}/SpringToolSuite4.exe
+${workdir}/.github/scripts/sign-exe.sh ./${sts_folder}/SpringToolSuite4.exe ${sts_folder}/SpringToolSuite4.exe
 zip -r -q $file ./${destination_folder_name}
+java -jar ${workdir}/.github/scripts/self-extracting-jar-creator.jar $file
+ls
