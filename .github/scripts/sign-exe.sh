8418807d106c3573a39a970113564c4b3ef63d54
aboyko <aboyko@vmware.com>
2023-07-13 13:57:54 -0400

    Attempt to pass win zip to the next GHA job

---
+++ .github/scripts/sign-exe.sh
@@ -0,0 +1,15 @@
+set -e
+in_file=$1
+out_file=$2
+
+in_filename="$(basename -- $in_file)"
+echo "Copying $in_filename to remote machine..."
+scp -i $SSH_KEY $in_file $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo
+echo "Signing $in_filename..."
+ssh -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com -- /build/apps/signing/signserver/signc -v --input=/opt/bamboo/$in_filename --keyid=authenticode_SHA2 --signmethod="winddk-8.1" --output=/opt/bamboo/$in_filename --hash sha256
+echo "Copying **signed** $in_filename back... (into $out_file)"
+scp -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com:/opt/bamboo/$in_filename $out_file
+echo "Cleaning up remote machine..."
+ssh -i $SSH_KEY $SSH_USER@vm-tools.spring.vmware.com -- rm /opt/bamboo/$in_filename
+echo "Successfully signed $in_filename"
+
