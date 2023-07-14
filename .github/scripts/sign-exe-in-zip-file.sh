c94242122e159ace6db9551fef896c47d9268dae
aboyko <aboyko@vmware.com>
2023-07-13 20:03:22 -0400

    GH Hosted: preparation for S3

--- .github/scripts/sign-exe-in-zip-file.sh
+++ .github/scripts/sign-exe-in-zip-file.sh
@@ -7,10 +7,19 @@ echo "****************************************************************"
 echo "*** Processing : ${file}"
 echo "****************************************************************"
 destination_folder_name=extracted_${filename}
+echo "Extracting archive ${filename}"
 unzip -q $file -d ./${destination_folder_name}
+echo "Successfully extracted ${filename}"
 sts_folder=`find ./${destination_folder_name} -maxdepth 1 -type d -name 'sts-*' -print -quit`
-echo "Found folder: ${sts_folder}"
+echo "Found STS distro folder: ${sts_folder}"
 ${workdir}/.github/scripts/sign-exe.sh ./${sts_folder}/SpringToolSuite4.exe ${sts_folder}/SpringToolSuite4.exe
+echo "Adding to zip contents of a folder ${destination_folder_name}"
 zip -r -q $file ./${destination_folder_name}
+echo "Successfully zipped ${destination_folder_name} into ${file}"
 java -jar ${workdir}/.github/scripts/self-extracting-jar-creator.jar $file
-ls
+
+filename_no_ext="${filename%.*}"
+echo "Creating checksums sha-256 and md5"
+sha256sum $file > ${file}.sha256
+md5sum $file > ${file}.md5
+
