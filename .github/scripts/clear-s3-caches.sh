ace0898259e2cbf1e4dee1c170af086ff555afcf
aboyko <aboyko@vmware.com>
2023-07-31 15:53:25 -0400

    [GHA-Build] debug clear cache script

--- .github/scripts/clear-s3-caches.sh
+++ .github/scripts/clear-s3-caches.sh
@@ -1,5 +1,5 @@
 s3_path=$1
-invalidation_json=`aws cloudfront create-invalidation --distribution-id ECAO9Q8651L8M --paths $s3_path`
+invalidation_json=`aws cloudfront create-invalidation --distribution-id ECAO9Q8651L8M --output json --paths "${s3_path}"`
 echo "Invalidation response: ${invalidation_json}"
 invalidation_id=`echo $invalidation_json | jq -r '.Invalidation.Id'`
 invalidation_status=`echo $invalidation_json | jq -r '.Invalidation.Status'`
@@ -8,6 +8,6 @@ while [ "${invalidation_status}" == "InProgress" ]
 do
    echo "Invalidation status: ${invalidation_status}"
    sleep 3
-   invalidation_status=`aws cloudfront get-invalidation --distribution-id ECAO9Q8651L8M --id $invalidation_id | jq -r '.Invalidation.Status'`
+   invalidation_status=`aws cloudfront get-invalidation --distribution-id ECAO9Q8651L8M --id $invalidation_id --output json | jq -r '.Invalidation.Status'`
 done
 echo "Final invalidation status: ${invalidation_status}"
