25318dc3b96e0bb3415fb07e8f28347c242ad102
aboyko <aboyko@vmware.com>
2023-07-31 16:02:07 -0400

    [GHA-Build] debug clear cache script

--- .github/scripts/clear-s3-caches.sh
+++ .github/scripts/clear-s3-caches.sh
@@ -1,5 +1,5 @@
 s3_path=$1
-aws cloudfront create-invalidation --distribution-id ECAO9Q8651L8M --output json --paths "${s3_path}/*"
+aws cloudfront create-invalidation --distribution-id ECAO9Q8651L8M --output json --paths 'release/TOOLS/sts4-language-server-integrations/2023-07-31/*'
 invalidation_json=`aws cloudfront create-invalidation --distribution-id ECAO9Q8651L8M --output json --paths "${s3_path}/*"`
 echo "Invalidation response: ${invalidation_json}"
 invalidation_id=`echo $invalidation_json | jq -r '.Invalidation.Id'`
