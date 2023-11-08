a037705f0bfc79a8770f781c9ba02de7761f91bb
aboyko <aboyko@vmware.com>
2023-11-08 17:29:30 -0500

    GHA: Upload ls extensions snapshot to Akamai

--- .github/scripts/akamai-aws-cli-env.sh
+++ .github/scripts/akamai-aws-cli-env.sh
@@ -2,7 +2,11 @@
 # The script asks for temp credentials to be able to upload to Akamai S3 origin bucket
 # The AWS CLI environment variables are then updated with temp credentials values
 session_name=$1
-duration_seconds=$2
+duration_seconds=900
+if [ ! -z "$2" ]; then
+  duration_seconds=$2
+fi
+
 export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
 $(aws sts assume-role \
 --role-arn arn:aws:iam::${{ secrets.TOOLS_CLOUDGATE_ACCOUNT_ID }}:role/${{ secrets.TOOLS_CLOUDGATE_USER }} \
