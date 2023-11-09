27fa449db1ab54c1a3baa74988ae32cb75ed85cc
aboyko <aboyko@vmware.com>
2023-11-08 19:21:28 -0500

    GHA: Correct akamai aws cli setup script

--- .github/scripts/akamai-aws-cli-env.sh
+++ .github/scripts/akamai-aws-cli-env.sh
@@ -9,7 +9,7 @@ fi
 
 export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
 $(aws sts assume-role \
---role-arn arn:aws:iam::${{ secrets.TOOLS_CLOUDGATE_ACCOUNT_ID }}:role/${{ secrets.TOOLS_CLOUDGATE_USER }} \
+--role-arn "arn:aws:iam::${TOOLS_CLOUDGATE_ACCOUNT_ID}:role/${TOOLS_CLOUDGATE_USER}" \
 --role-session-name $session_name \
 --duration-seconds $duration_seconds \
 --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
