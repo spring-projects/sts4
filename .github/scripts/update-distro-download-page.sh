0673e24f003fc07a100568ae240c5e6fcd64d16f
aboyko <aboyko@vmware.com>
2023-11-09 16:57:07 -0500

    GHA: Upload to S3 and Akamai

--- .github/scripts/update-distro-download-page.sh
+++ .github/scripts/update-distro-download-page.sh
@@ -2,14 +2,14 @@ dist_path=$1
 eclipse_version=$2
 build_type=$3
 
-s3_url=s3://dist.springsource.com/${dist_path}
+s3_url=s3://tools-spring-io/${dist_path}
 downloads_html="sts4-nightly-${eclipse_version}.html"
 files=`aws s3 cp ${s3_url} . --recursive --exclude "*" --include "spring-tool-suite-4*.zip" --include "spring-tool-suite-4*.dmg" --include "spring-tool-suite-4*.self-extracting.jar" --include "spring-tool-suite-4*.tar.gz" --exclude "*/*" --dryrun`
 rm -f ./${downloads_html}
 echo '<ul>' >> $downloads_html
 for file in $files
 do
-  if [[ "$file" =~ ^"s3://dist." ]]; then
+  if [[ "$file" =~ ^"s3://tools-spring-io" ]]; then
     download_url=https://download${file:9}
     filename=${file:${#s3_url}+1}
     echo '  <li><a href="'${download_url}'">'${filename}'</li>' >> $downloads_html
@@ -17,4 +17,5 @@ do
 done
 echo '</ul>' >> $downloads_html
 cat ./$downloads_html
-aws s3 mv ./$downloads_html s3://dist.springsource.com/${build_type}/STS4/ --acl public-read --no-progress
+aws s3 cp ./$downloads_html s3://dist.springsource.com/${build_type}/STS4/ --acl public-read --no-progress
+aws s3 mv ./$downloads_html s3://tools-spring-io/${build_type}/STS4/ --no-progress
