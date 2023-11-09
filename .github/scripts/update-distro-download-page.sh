2a493dbbb01882d9e1cd22acd755e9d03202f1ee
aboyko <aboyko@vmware.com>
2023-11-09 17:41:23 -0500

    GHA: Revert downloads page script changes. Update all to Cloudgate. Concourse

--- .github/scripts/update-distro-download-page.sh
+++ .github/scripts/update-distro-download-page.sh
@@ -2,14 +2,14 @@ dist_path=$1
 eclipse_version=$2
 build_type=$3
 
-s3_url=s3://tools-spring-io/${dist_path}
+s3_url=s3://dist.springsource.com/${dist_path}
 downloads_html="sts4-nightly-${eclipse_version}.html"
 files=`aws s3 cp ${s3_url} . --recursive --exclude "*" --include "spring-tool-suite-4*.zip" --include "spring-tool-suite-4*.dmg" --include "spring-tool-suite-4*.self-extracting.jar" --include "spring-tool-suite-4*.tar.gz" --exclude "*/*" --dryrun`
 rm -f ./${downloads_html}
 echo '<ul>' >> $downloads_html
 for file in $files
 do
-  if [[ "$file" =~ ^"s3://tools-spring-io" ]]; then
+  if [[ "$file" =~ ^"s3://dist." ]]; then
     download_url=https://download${file:9}
     filename=${file:${#s3_url}+1}
     echo '  <li><a href="'${download_url}'">'${filename}'</li>' >> $downloads_html
