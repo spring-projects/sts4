b3746ac23d42e2cbc6dc4988cef6ccd48361ef84
Martin Lippert <martin.lippert@broadcom.com>
2024-04-11 09:59:03 +0200

    update to e4.31 as the main build target

--- eclipse-distribution/local-build.sh
+++ eclipse-distribution/local-build.sh
@@ -1,2 +1,2 @@
-./mvnw clean install -Pe430 -Psnapshot -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5
+./mvnw clean install -Pe431 -Psnapshot -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5
 
