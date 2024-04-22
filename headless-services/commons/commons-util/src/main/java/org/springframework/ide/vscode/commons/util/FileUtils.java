4e73ce69ccd053de86cca501522d949e53c6aca7
Martin Lippert <martin.lippert@broadcom.com>
2024-04-22 09:34:03 +0200

    fix typo

--- headless-services/commons/commons-util/src/main/java/org/springframework/ide/vscode/commons/util/FileUtils.java
+++ headless-services/commons/commons-util/src/main/java/org/springframework/ide/vscode/commons/util/FileUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2017 Pivotal, Inc.
+ * Copyright (c) 2016, 2024 Pivotal, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,7 +18,7 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
- * Utilitity methods for working with files
+ * Utility methods for working with files
  * 
  * @author Kris De Volder
  * @author Alex Boyko
