ce0fccee9abbdef1d66c0b8e242f0d9e98ef7256
aboyko <alex.boyko@broadcom.com>
2024-05-14 15:03:49 -0400

    Retain original copyright headers

--- headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/semantictokens/SemanticTokensVisitor.java
+++ headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/semantictokens/SemanticTokensVisitor.java
@@ -1,12 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2024 Broadcom, Inc.
+ * Copyright (c) 2020 Microsoft Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     Broadcom, Inc. - initial API and implementation
+ *     Microsoft Corporation - initial API and implementation
+ *     0dinD - Semantic highlighting improvements - https://github.com/eclipse/eclipse.jdt.ls/pull/1501
  *******************************************************************************/
 // Copied from JDT LS to workaround https://github.com/spring-projects/sts4/issues/1249
 package org.springframework.ide.vscode.boot.java.semantictokens;
