884b2c6953b263227b1e8c8afc493c81283418ce
vudayani <vudayani@vmware.com>
2024-05-31 12:15:09 +0530

    add generate test command and related config

--- headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/app/CommandsConfig.java
+++ headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/app/CommandsConfig.java
@@ -15,6 +15,7 @@ import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ide.vscode.boot.app.ai.VectorStoreHandler;
+import org.springframework.ide.vscode.boot.index.SpringMetamodelIndex;
 import org.springframework.ide.vscode.boot.java.commands.WorkspaceBootExecutableProjects;
 import org.springframework.ide.vscode.commons.languageserver.java.JavaProjectFinder;
 import org.springframework.ide.vscode.commons.languageserver.util.SimpleLanguageServer;
@@ -22,8 +23,8 @@ import org.springframework.ide.vscode.commons.languageserver.util.SimpleLanguage
 @Configuration(proxyBeanMethods = false)
 public class CommandsConfig {
 	
-	@Bean WorkspaceBootExecutableProjects workspaceBootProjects(SimpleLanguageServer server, JavaProjectFinder projectFinder, SpringSymbolIndex symbolIndex) {
-		return new WorkspaceBootExecutableProjects(server, projectFinder, symbolIndex);
+	@Bean WorkspaceBootExecutableProjects workspaceBootProjects(SimpleLanguageServer server, JavaProjectFinder projectFinder, SpringSymbolIndex symbolIndex, SpringMetamodelIndex springMetamodelIndex) {
+		return new WorkspaceBootExecutableProjects(server, projectFinder, symbolIndex, springMetamodelIndex);
 	}
 	
 	@Bean VectorStoreHandler vectorStoreHandler(SimpleLanguageServer server, VectorStore vectorStore, ChatClient aiClient) {
