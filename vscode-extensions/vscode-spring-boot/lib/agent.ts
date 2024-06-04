815b300118c446de422548cb587f22c0eb86b12a
vudayani <vudayani@vmware.com>
2024-06-04 10:48:14 +0530

    clean up /add command, remove vector search

--- vscode-extensions/vscode-spring-boot/lib/agent.ts
+++ vscode-extensions/vscode-spring-boot/lib/agent.ts
@@ -11,8 +11,6 @@ import { getTargetGuideMardown, getWorkspaceRoot, getExecutable } from './utils/
 import { createConverter } from "vscode-languageclient/lib/common/protocolConverter";
 import { systemPrompt } from './utils/system-ai-prompt';
 import { userPrompt } from './utils/user-ai-prompt';
-import { vectorSearchPrompt } from './utils/vectorsearch-ai-prompt';
-import { systemAiTestPrompt } from './utils/system-ai-test-prompt';
 
 interface Prompt {
     projName: string
@@ -38,7 +36,7 @@ interface ExecutableBootProject {
 }
 
 const CONVERTER = createConverter(undefined, true, true);
-const MODEL_SELECTOR: vscode.LanguageModelChatSelector = { vendor: 'copilot', family: 'copilot-gpt-4' };
+const MODEL_SELECTOR: vscode.LanguageModelChatSelector = { vendor: 'copilot', family: 'gpt-3.5-turbo' };
 const AGENT_ID = 'springboot';
 
 interface SpringBootChatAgentResult extends vscode.ChatResult {
@@ -145,15 +143,7 @@ async function enhancePrompt(question: string, cwd: string, projects: Executable
 
     const match = projects.find(project => project.uri.toString().replace('file:', '') === cwd);
     const prompt = {} as Prompt;
-    if (Number(match.javaVersion) >= 17 && match.springBootVersion.startsWith('3') && question.includes('jpa')) {
-        const vectorSearchResults = await vscode.commands.executeCommand("sts/copilot/search", {"question": question}) as string[];
-        console.log(vectorSearchResults[0]);
-        console.log(vectorSearchResults[1]);
-        // const joinedVectorSearchResults = vectorSearchResults.join('\n');
-        prompt.systemPrompt = replacePlaceholder(vectorSearchPrompt, question, match , vectorSearchResults[0]);
-    } else {
-        prompt.systemPrompt = replacePlaceholder(systemPrompt, question, match);
-    } 
+    prompt.systemPrompt = replacePlaceholder(systemPrompt, question, match); 
     prompt.userPrompt = replacePlaceholder(userPrompt, question, match);
     prompt.projName = match?.name;
     return Promise.resolve(prompt);
@@ -239,6 +229,7 @@ async function chatRequest(enhancedPrompt: Prompt, token: vscode.CancellationTok
             let chatResponse: vscode.LanguageModelChatResponse | undefined;
             try {
                 const [model] = await vscode.lm.selectChatModels(MODEL_SELECTOR);
+                console.log(model)
                 if(model) {
                     chatResponse = await model.sendRequest(messages, {}, token);
                 }      
@@ -266,96 +257,25 @@ async function chatRequest(enhancedPrompt: Prompt, token: vscode.CancellationTok
 
 async function handleAiPrompts(request: vscode.ChatRequest, context: vscode.ChatContext, stream: vscode.ChatResponseStream, token: vscode.CancellationToken): Promise<SpringBootChatAgentResult> {
 
-    const previousMessages = context.history.filter(h => {
-        return h instanceof vscode.ChatRequestTurn && h.participant == AGENT_ID
-    }) as vscode.ChatRequestTurn[];
-    console.log(previousMessages);
-
-    const previousMessagesResp = context.history.filter(h => {
-        return h instanceof vscode.ChatResponseTurn && h.participant == AGENT_ID
-    }) as vscode.ChatResponseTurn[];
-    console.log(previousMessagesResp);
-
     const cwd = (await getWorkspaceRoot()).fsPath;
 
     const projects = await vscode.commands.executeCommand("sts/spring-boot/executableBootProjects") as ExecutableBootProject[];
     console.log(projects);
-
-    // const vectorSearchResults = await vscode.commands.executeCommand("sts/copilot/search", {"question": request.prompt}) as string[];
-    // console.log(vectorSearchResults[0]);
-    // console.log(vectorSearchResults[1]);
 
     // get enhanced prompt by getting the spring context from boot ls and adding vector search results
     const enhancedPrompt = await enhancePrompt(request.prompt, cwd, projects);
     console.log(enhancedPrompt.systemPrompt);
     console.log(enhancedPrompt.userPrompt);
+
     // chat request to copilot LLM
     const response = await chatRequest(enhancedPrompt, token, request.prompt);
 
     // write the response to markdown file
-    // const match = projects.find(project => project.uri.toString().replace('file:', '') === cwd);
-    // if(match !== null && match !== undefined && match.springBootVersion.startsWith('3')) {
-        await writeResponseToFile(response, enhancedPrompt.projName, cwd);
-    // }
+    await writeResponseToFile(response, enhancedPrompt.projName, cwd);
 
     const uri = await getTargetGuideMardown();
     // modify the response from copilot LLM i.e. make response Boot 3 compliant if necessary
-    // await enhanceResponse(uri, enhancedPrompt.projName, cwd);
-
-    // return modified response to chat
-    const documentContent = await vscode.workspace.fs.readFile(uri);
-    const chatResponse = Buffer.from(documentContent).toString();
-    stream.markdown(chatResponse);
-    stream.button({
-        command: 'vscode-spring-boot.agent.apply',
-        title: vscode.l10n.t('Apply Changes!')
-    });
-    return { metadata: { command: 'prompt' } };
-}
-
-async function generateTestCases(request: vscode.ChatRequest, context: vscode.ChatContext, stream: vscode.ChatResponseStream, token: vscode.CancellationToken): Promise<SpringBootChatAgentResult> {
-
-    const cwd = (await getWorkspaceRoot()).fsPath;
-
-    const projects = await vscode.commands.executeCommand("sts/spring-boot/executableBootProjects") as ExecutableBootProject[];
-    console.log(projects);
-
-    let activeEditor = vscode.window.activeTextEditor;
-    let beans;
-    if (activeEditor) {
-        let activeDocumentUri = activeEditor.document.uri;
-        console.log(activeDocumentUri.toString());
-        beans = await vscode.commands.executeCommand("sts/spring-boot/getBeans", {"docUri": activeDocumentUri.toString()}) as ExecutableBootProject[];
-        console.log(beans);
-    } else {
-        console.log('No active editor');
-    }
-
-    const vectorSearchResults = await vscode.commands.executeCommand("sts/copilot/search", {"question": request.prompt}) as string[];
-
-    // const vectorSearchResults = await vscode.commands.executeCommand("sts/copilot/search", {"question": request.prompt}) as string[];
-    // // console.log(vectorSearchResults[0]);
-    // // console.log(vectorSearchResults[1]);
-
-    // // get enhanced prompt by getting the spring context from boot ls and adding vector search results
-    // const enhancedPrompt = await enhancePrompt(request.prompt, cwd, projects);
-    // console.log(enhancedPrompt.systemPrompt);
-    // console.log(enhancedPrompt.userPrompt);
-    // // chat request to copilot LLM
-    const prompt = {} as Prompt;
-    prompt.systemPrompt = systemAiTestPrompt + '\n' + 'Generate test cases for classes. Autowire the beans in the class for test cases. Below is the bean information' + `\n` + JSON.stringify(beans) + '\n Reference: '+ vectorSearchResults[0];
-    prompt.userPrompt = userPrompt + '\n' + 'Generate detailed test cases for class: ' + '\n' + JSON.stringify(request.references[0].value);
-    prompt.projName = 'testcases';
-    console.log(prompt.systemPrompt);
-    console.log(prompt.userPrompt);
-    const response = await chatRequest(prompt, token, request.prompt);
-
-    // write the response to markdown file
-    await writeResponseToFile(response, prompt.projName, cwd);
-
-    const uri = await getTargetGuideMardown();
-    // // modify the response from copilot LLM i.e. make response Boot 3 compliant if necessary
-    // // await enhanceResponse(uri, enhancedPrompt.projName, cwd);
+    // await enhanceResponse(uri, enhancedPrompt.shortPackageName, cwd);
 
     // return modified response to chat
     const documentContent = await vscode.workspace.fs.readFile(uri);
@@ -378,12 +298,8 @@ export function activate(
 
         if (request.command === 'add') {
             return handleAiPrompts(request, context, progress, token);
-        } else if (request.command === 'generate') {
-            return generateTestCases(request, context, progress, token);
         }
         return handleAiPrompts(request, context, progress, token);
     });
     agent.iconPath = vscode.Uri.joinPath(context.extensionUri, 'readme-imgs', 'spring-tools-icon.png');
 }
-
-
