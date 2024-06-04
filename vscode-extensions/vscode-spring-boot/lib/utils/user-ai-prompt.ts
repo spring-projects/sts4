c1e44c73f76a00310396e239c1ccd960a2f8f574
vudayani <vudayani@vmware.com>
2024-06-04 10:48:14 +0530

    clean up /add command, remove vector search

--- vscode-extensions/vscode-spring-boot/lib/utils/user-ai-prompt.ts
+++ vscode-extensions/vscode-spring-boot/lib/utils/user-ai-prompt.ts
@@ -1,15 +1,13 @@
 export const userPrompt = `Create the Spring Java application with the following project information
 
 Main Spring project: [Spring Project Name]
-Package name: [Package Name]
+Root Package name: [Package Name]
 Build tool: [Build Tool]
 Java version: [Java Version]
 Spring boot version: [Spring Boot Version]
 
 Ensure the solution meets the following additional criteria
 
-1. Include import statements in all code files.
-2. Include getters and setters whereever necessary.
+1. Include import statements in all code files, getter and setters in entity classes.
 2. When generating markdown for the 'application.properties' file in Spring Boot, please ensure that the markdown 'info' field is set to 'properties' for that specific section.
-3. Use the Package name as the root package and place the files in sub-packages accordingly.
 `
