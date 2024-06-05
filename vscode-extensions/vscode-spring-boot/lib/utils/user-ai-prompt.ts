b6c9ba7bf48d17ffdb490bce8b2b3eb5ca64feb8
vudayani <vudayani@vmware.com>
2024-06-05 12:27:44 +0530

    minor additions to user prompts

--- vscode-extensions/vscode-spring-boot/lib/utils/user-ai-prompt.ts
+++ vscode-extensions/vscode-spring-boot/lib/utils/user-ai-prompt.ts
@@ -8,6 +8,8 @@ Spring boot version: [Spring Boot Version]
 
 Ensure the solution meets the following additional criteria
 
-1. Include import statements in all code files, getter and setters in entity classes.
-2. When generating markdown for the 'application.properties' file in Spring Boot, please ensure that the markdown 'info' field is set to 'properties' for that specific section.
+1. Include import statements in all code files
+2. Generate getter and setters in entity classes
+3. Provide the necessary Spring Boot Starter dependencies in the pom.xml file 
+4. When generating markdown for the 'application.properties' file in Spring Boot, please ensure that the markdown 'info' field is set to 'properties' for that specific section.
 `
