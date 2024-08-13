803b27d2bc3ff2fb042c8e9812e9a9230442b242
ksankaranara <karthik.sankaranarayanan@broadcom.com>
2024-08-13 13:59:45 +0530

    Update copyright to Broadcom

--- headless-services/spring-boot-language-server/src/test/java/org/springframework/ide/vscode/boot/java/contextconfiguration/test/ContextConfigurationCompletionTest.java
+++ headless-services/spring-boot-language-server/src/test/java/org/springframework/ide/vscode/boot/java/contextconfiguration/test/ContextConfigurationCompletionTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2024 Pivotal, Inc.
+ * Copyright (c) 2017, 2024 Broadcom, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Pivotal, Inc. - initial API and implementation
+ *     Broadcom, Inc. - initial API and implementation
  *******************************************************************************/
 package org.springframework.ide.vscode.boot.java.contextconfiguration.test;
 
