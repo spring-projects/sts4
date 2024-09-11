dc577efc87f675a739b554910800968e9173839c
aboyko <alex.boyko@broadcom.com>
2024-09-11 13:47:40 -0400

    Bump up version 4.25.0 -> 4.26.0

--- eclipse-distribution/local-build.sh
+++ eclipse-distribution/local-build.sh
@@ -1,2 +1,2 @@
-./mvnw clean install -Pe432 -Psnapshot -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5
+./mvnw clean install -Pe433 -Psnapshot -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5
 
