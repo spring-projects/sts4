ae799a737fe6f173cd450c11ee9df6c6ebb259d9
aboyko <alex.boyko@broadcom.com>
2024-11-06 21:52:30 -0500

    [GHA] corrections

--- .github/scripts/sign-exe.sh
+++ .github/scripts/sign-exe.sh
@@ -3,13 +3,14 @@ in_file=$1
 out_file=$2
 id=$3
 wait_time=$4
+number_of_sleeps=$5
 
 in_filename="$(basename -- $in_file)"
 echo "Copying ${in_file} to s3 s3://${AWS_S3_BUCKET}/exes-to-sign/${id}.exe for signing"
 aws s3 cp $in_file s3://$AWS_S3_BUCKET/exes-to-sign/$id.exe
-for i in {1..10}
+for i in {1..$number_of_sleeps}
 do
-  wait $wait_time
+  sleep $wait_time
   aws s3api head-object --bucket $CDN_BUCKET --key spring-tools/exes-signed/$id.exe || NOT_EXIST=true
   if [ $NOT_EXIST ]; then
     echo "Waited ${wait_time} seconds but ${in_filename} hasn't been signed yet..."
