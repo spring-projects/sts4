195443360ca682cb031364a04b55e35184784afa
Martin Lippert <martin.lippert@broadcom.com>
2024-11-18 15:02:32 +0100

    added missing copyright note

--- headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/reconcilers/ImplicitWebAnnotationNamesReconciler.java
+++ headless-services/spring-boot-language-server/src/main/java/org/springframework/ide/vscode/boot/java/reconcilers/ImplicitWebAnnotationNamesReconciler.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Broadcom
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Broadcom - initial API and implementation
+ *******************************************************************************/
 package org.springframework.ide.vscode.boot.java.reconcilers;
 
 import static org.springframework.ide.vscode.commons.java.SpringProjectUtil.springBootVersionGreaterOrEqual;
