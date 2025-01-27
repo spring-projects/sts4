40b622e4f540a87dd52d1b9970cfb8de2a905bf0
Martin Lippert <martin.lippert@broadcom.com>
2025-01-27 15:26:27 +0100

    add request mapping details as index child elements to controller bean

--- headless-services/spring-boot-language-server/src/test/resources/test-projects/test-request-mapping-symbols/src/main/java/org/test/inheritance/ControllerAsSubclassAndInterfaceHierarchy.java
+++ headless-services/spring-boot-language-server/src/test/resources/test-projects/test-request-mapping-symbols/src/main/java/org/test/inheritance/ControllerAsSubclassAndInterfaceHierarchy.java
@@ -1,7 +1,9 @@
 package org.test.inheritance;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ControllerAsSubclassAndInterfaceHierarchy extends SuperclassWithMappingPath implements EmptyInterfaceWithinHierarchy {
 	
 	@GetMapping("last-path-segment")
