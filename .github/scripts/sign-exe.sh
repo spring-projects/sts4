0d8a273beee29482d809ec659699c780c8a13602
aboyko <alex.boyko@broadcom.com>
2025-02-11 20:42:17 -0500

    [GHA] Try to fix the build

--- .github/scripts/sign-exe.sh
+++ .github/scripts/sign-exe.sh
@@ -7,7 +7,7 @@ timeout=$5
 
 in_filename="$(basename -- $in_file)"
 echo "Copying ${in_file} to s3 s3://${AWS_S3_BUCKET}/exes-to-sign/${id}.exe for signing"
-aws s3 cp $in_file s3://$AWS_S3_BUCKET/exes-to-sign/$id.exe --no-progress
+aws s3 cp $in_file s3://$AWS_S3_BUCKET/exes-to-sign/$id.exe --no-progress  --checksum-algorithm CRC32
 for (( i=wait_time; i<timeout; i+=wait_time )) ; {
   sleep $wait_time
   aws s3api head-object --bucket $CDN_BUCKET --key spring-tools/exes-signed/$id.exe >/dev/null 2>&1 || not_exist=true
