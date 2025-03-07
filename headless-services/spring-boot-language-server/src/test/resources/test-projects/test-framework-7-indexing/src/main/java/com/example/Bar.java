4ffdb359428322fe0a25a21c4dd6698c4ae3d1ca
Martin Lippert <martin.lippert@broadcom.com>
2025-03-07 13:00:01 +0100

    GH-1498: created early test project for indexing framework 7 bean registrars

---
+++ headless-services/spring-boot-language-server/src/test/resources/test-projects/test-framework-7-indexing/src/main/java/com/example/Bar.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Bar {
+	
+	private Foo foo;
+
+	public Bar(Foo foo) {
+		this.foo = foo;
+	}
+	
+	public Foo getFoo() {
+		return foo;
+	}
+
+}
---
+++ headless-services/spring-boot-language-server/src/test/resources/test-projects/test-framework-7-indexing/src/main/java/com/example/Baz.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Baz {
+	
+	private String something;
+
+	public Baz(String something) {
+		this.something = something;
+	}
+	
+	public void printSomething() {
+		System.out.println(something);
+	}
+
+}
