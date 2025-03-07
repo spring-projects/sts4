0f759b160e4de9e9c734a4ef495d15b7706a18e9
Martin Lippert <martin.lippert@broadcom.com>
2025-03-07 13:00:01 +0100

    GH-1498: created early test project for indexing framework 7 bean registrars

---
+++ headless-services/spring-boot-language-server/src/test/resources/test-projects/test-framework-7-indexing/src/main/java/com/example/MyBeanRegistrar.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.beans.factory.BeanRegistrar;
+import org.springframework.beans.factory.BeanRegistry;
+import org.springframework.core.env.Environment;
+
+public class MyBeanRegistrar implements BeanRegistrar {
+
+	@Override
+	public void register(BeanRegistry registry, Environment env) {
+		registry.registerBean(FooFoo.class);
+        registry.registerBean("foo", Foo.class);
+        registry.registerBean("bar", Bar.class, spec -> spec
+                .prototype()
+                .lazyInit()
+                .description("Custom description")
+                .supplier(context -> new Bar(context.bean(Foo.class))));
+        if (env.matchesProfiles("baz")) {
+            registry.registerBean(Baz.class, spec -> spec
+                    .supplier(context -> new Baz("Hello World!")));
+        }
+	}
+
+}
