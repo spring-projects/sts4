9a90390fe0a6d4a27d4273ad41cc627e845b7f72
Alex Boyko <alex.boyko@broadcom.com>
2025-03-13 03:39:20 -0400

    GH-1503: update everything to Java 21
    * Switch to JDT LS 1.45 and Java 21
    * Java 21 for LS side
    * Switch to Java 21 the branding plugin
    
    ---------
    
    Signed-off-by: Martin Lippert <martin.lippert@broadcom.com>
    Co-authored-by: Alex Boyko <alex.boyko@broadcom.com>

--- eclipse-distribution/local-build.sh
+++ eclipse-distribution/local-build.sh
@@ -1,2 +1,2 @@
-./mvnw clean install -Pe436 -Psnapshot -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5
+./mvnw clean install -Pe434 -Psnapshot -Dhttpclient.retry-max=20 -Dmaven.test.skip=true -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dorg.eclipse.equinox.p2.transport.ecf.retry=5
 
--- headless-services/README.md
+++ headless-services/README.md
@@ -1,2 +1,3 @@
+
 # headless services
 The main parts of the tooling for Spring Boot apps are implemented as headless services, running in separate processes. Those services can be consumed in existing IDEs by using the Language Server Protocol from Visual Studio Code.
